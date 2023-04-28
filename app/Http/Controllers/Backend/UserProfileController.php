f7723acb6e6e100ab53b388ea20ee7ec7f2a08a7
mdparvej123 <pj.parvaz45@gmail.com>
2023-04-28 23:02:56 +0600

    bug fix and responsive fix

--- app/Http/Controllers/Backend/UserProfileController.php
+++ app/Http/Controllers/Backend/UserProfileController.php
@@ -60,12 +60,15 @@ class UserProfileController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'user_name' => ['required', 'string', 'max:255'],
-            'phone' => ['required', 'string'],
+            'phone' => ['required', 'max:11', 'min:11'],
+            'profile_img' => ['image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
 
             
         ]);
         if ($request->hasFile('profile_img')) {
-            $path = 'profile/' . $userData->image;
+            $image_explode = explode('/', $userData->image_url);
+            $old_img_name = end($image_explode);
+            $path = 'profile/' . $old_img_name;
             if (Storage::disk('public')->exists($path)) {
                 Storage::disk('public')->delete($path);
             }
@@ -73,11 +76,10 @@ class UserProfileController extends Controller
             $userData->name = $request->name;
             $userData->user_name = $request->user_name;
             $userData->phone = $request->phone;
-            $userData->image = $saveImage['image_url'];
             $userData->image_url = $saveImage['image_url'];
             $userData->save();
             $notification = [
-                'message' => 'Admin Profile Successfully Updated',
+                'message' => 'Profile Successfully Updated',
                 'alert-type' => 'success',
             ];
             return redirect()
@@ -89,7 +91,7 @@ class UserProfileController extends Controller
             $userData->phone = $request->phone;
             $userData->save();
             $notification = [
-                'message' => 'Admin Profile Successfully Updated',
+                'message' => 'Profile Successfully Updated',
                 'alert-type' => 'success',
             ];
             return redirect()
--- resources/views/backend/profile/profile-edit.blade.php
+++ resources/views/backend/profile/profile-edit.blade.php
@@ -6,28 +6,67 @@
     @csrf
     @method('PUT')  
 <div class="container rounded bg-white mb-5">
-    <div class="row">
-            <div class="col-md-3 mt-4 border-right">
+      <div class="row">
+        <div class="d-flex justify-content-between align-items-center mt-4">
+            <h4 class="text-right">Profile Update</h4>
+        </div>
+            <div class="col-md-3 mt-1 border-right">
                 <label style="width: 9rem; height:9rem; margin-left: 3rem"  for="imagefile">
-                    <input type="file" id="imagefile" style=" visibility:hidden;" name="profile_img" >
+                    <input class="form-control
+                    @error('profile_img')
+                    is-invalid
+                    @enderror
+                    " type="file" id="imagefile" style=" visibility:hidden;" name="profile_img" >
                     <img class="mt-1" id="liveImage" style=" height:100%; border:1px solid rgb(199, 199, 199); width:100%; border-radius:50%;" src="{{ $user->image_url }}" alt="">
-                    <div class="row col-sm-12">
+                    @error('profile_img')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+
+
+                    <div class="row m-2 col-sm-12 text-center">
                         <span class="col-sm-12 font-weight-bold">{{ $user->name }}</span>
-                        <span class="col-sm-12 text-black-50">{{ $user->email }}</span><span> </span>
+                        <span class="col-sm-12 text-black-50" style="pading-right: 3rem;">{{ $user->email }}</span>
                     </div>
                 </label>
             </div>
             <div class="col-md-9 border-right">
-                <div class="p-2 py-5">
-                    <div class="d-flex justify-content-between align-items-center mb-1">
-                        <h4 class="text-right">Profile Update</h4>
+                <div class=" py-5">
+                    <div class="row">
+                        <div class="col-md-6"><label class="labels">Name<span style="color:red;">*</span></label><input name="name" type="text" class="form-control 
+                            @error('name')
+                            is-invalid
+                            @enderror" placeholder="fullname" value="{{ $user->name }}"> 
+                            @error('name')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                            </div>
+
+                        
+                        <div class="col-md-6"><label class="labels">Username<span style="color:red;">*</span></label><input name="user_name" type="text" class="form-control 
+                            @error('user_name')
+                            is-invalid
+                            @enderror
+                            " value="{{ $user->user_name }}" placeholder="username">
+                            @error('user_name')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+
                     </div>
-                    <div class="row mt-2">
-                        <div class="col-md-6"><label class="labels">Name<span style="color:red;">*</span></label><input name="name" type="text" class="form-control" placeholder="fullname" value="{{ $user->name }}"></div>
-                        <div class="col-md-6"><label class="labels">Username<span style="color:red;">*</span></label><input name="user_name" type="text" class="form-control" value="{{ $user->user_name }}" placeholder="username"></div>
-                    </div>
-                    <div class="row mt-2">
-                        <div class="col-md-6"><label class="labels">Mobile Number<span style="color:red;">*</span></label><input name="phone" type="number" class="form-control" placeholder="phone number" value="{{ $user->phone }}"></div>
+                    <div class="row mt-3">
+                        <div class="col-md-6"><label class="labels">Mobile Number<span style="color:red;">*</span></label><input name="phone" type="number" class="form-control
+                            @error('phone')
+                            is-invalid
+                            @enderror
+                            " placeholder="phone number" value="{{ $user->phone }}">
+                            @error('phone')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+
+
                         <div class="col-md-6"><label class="labels">E-mail   <span style="color:green;"><small>  verified</small></span></label><input name="email" type="email" class="form-control" placeholder="email" disabled value="{{ $user->email }}"></div>
                     </div>
                     <div class="mt-3 text-center"><button class="btn btn-primary profile-button" type="submit" >Profile Update</button></div>
