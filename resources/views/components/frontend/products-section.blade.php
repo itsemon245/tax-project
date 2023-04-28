05f2d0e263b3ba8f00e980946b8440fcbed0aabe
Mojahidul Islam <bdemon245@gmail.com>
2023-04-28 20:57:10 +0600

    has an error regarding component location

--- resources/views/components/frontend/products-section.blade.php
+++ resources/views/components/frontend/products-section.blade.php
@@ -23,50 +23,7 @@
         </ul>
         <div class="tab-content">
             <div class="tab-pane active" id="home1" role="tabpanel">
-                <div class="product-card">
-                    <div class="product-type bg-blue p-1 text-light text-center h6 mb-0">Most Popular</div>
-                    <div class="product-content p-3">
-                        <h3 class="header-title h4 text-uppercase mb-0">
-                            Free
-                        </h3>
-                        <p class="h6">
-                            <span style="background: #f1556c" class="badge p-2">Free</span> expert assist
-                            included
-                        </p>
-                        <h4 class="product-price mb-0">Tk. 1000.</h4>
-                        <p class="product-discount mb-0">Early Bird Savings
-                            <span style="background:#f1556c;" class="badge p-2">44% Off</span>
-                        </p>
-
-                        <div class="d-flex justify-content-center mt-4">
-                            <div class="cta d-flex flex-column align-itmes-center">
-                                <div class="">
-                                    <button class="btn btn-success text-light" style="font-weight: 600">Start
-                                        Now</button>
-                                </div>
-                                <div class="rating">
-                                    <p class="text-muted float-start me-1 mb-0">
-                                        <span class="mdi mdi-star text-warning"></span>
-                                        <span class="mdi mdi-star text-warning"></span>
-                                        <span class="mdi mdi-star text-warning"></span>
-                                        <span class="mdi mdi-star text-warning"></span>
-                                        <span class="mdi mdi-star"></span>
-                                    </p>
-                                </div>
-                                <p class=""><a href="" class="text-muted">( 36 Reviews )</a></p>
-
-                            </div>
-                        </div>
-
-                        <div class="product-details">
-                            <ul class="custom-bullet">
-                                <li><span class="mdi mdi-check text-success"></span> Lorem ipsum dolor sit amet consectetur adipisicing</li>
-                                <li><span class="mdi mdi-check text-success"></span> Lorem ipsum dolor sit amet consectetur adipisicing</li>
-                                <li><span class="mdi mdi-check text-success"></span> Lorem ipsum dolor sit amet consectetur adipisicing</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                {{ $slot }}
             </div>
             <div class="tab-pane show" id="profile1" role="tabpanel">
                 <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
--- resources/views/frontend/pages/welcome.blade.php
+++ resources/views/frontend/pages/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('frontend.layouts.app')
 @section('main')
     <x-frontend.hero-section />
-    <x-frontend.products-section />
+    <x-frontend.products-section>
+        <x-frontend.product.card />
+    </x-frontend.products-section>
 @endsection
