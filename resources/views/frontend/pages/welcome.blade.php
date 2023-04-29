0e97c5c96b8ed7de5e87c804807a536cbff2c494
Mojahidul Islam <bdemon245@gmail.com>
2023-04-29 15:23:11 +0600

    minor changes in product section component

--- resources/views/frontend/pages/welcome.blade.php
+++ resources/views/frontend/pages/welcome.blade.php
@@ -1,7 +1,5 @@
 @extends('frontend.layouts.app')
 @section('main')
     <x-frontend.hero-section />
-    <x-frontend.products-section>
-        
-    </x-frontend.products-section>
+    <x-frontend.products-section />
 @endsection
