c61e81cf52d28d75256237816ff2c92257a578d3
Mojahidul Islam <bdemon245@gmail.com>
2023-04-29 11:28:10 +0600

    added files for Product Category & SubCategory

---
+++ app/Http/Controllers/Backend/Product/ProductSubCategoryController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Product;
+
+use App\Models\ProductSubCategory;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductSubCategoryRequest;
+use App\Http\Requests\UpdateProductSubCategoryRequest;
+
+class ProductSubCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductSubCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductSubCategoryRequest $request, ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+}
