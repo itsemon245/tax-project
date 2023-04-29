34c0cb3295671f71ede2dcf2a939047dc6da7cef
zabiremu <zabirraihan@gmail.com>
2023-04-29 20:30:20 +0600

    fix markup bugs

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -50,7 +50,6 @@
                 <li>
                     <a href="#sidebarDashboards" data-bs-toggle="collapse">
                         <i class="mdi mdi-view-dashboard-outline"></i>
-                        <span class="badge bg-success rounded-pill float-end">4</span>
                         <span> Dashboards </span>
                     </a>
                     <div class="collapse" id="sidebarDashboards">
