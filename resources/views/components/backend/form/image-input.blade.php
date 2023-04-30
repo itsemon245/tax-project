74599f820d5b4f0abaa6209e10934c776af2a6f0
Mojahidul Islam <bdemon245@gmail.com>
2023-05-01 03:15:15 +0600

    fixed image input

--- resources/views/components/backend/form/image-input.blade.php
+++ resources/views/components/backend/form/image-input.blade.php
@@ -4,8 +4,8 @@
     $note = $attributes->has('note') ? $attributes->get('note') : '<span class="text-danger" style="font-weight: 500;">*</span>Accepted files : <span class="text-success" style="font-weight: 500;">jpg, png, svg, webp up to 5 MB</span> ';
 @endphp
 <div class="">
-    <label for="{{ $name }}">
-        <input id="{{ $name }}" name="{{ $name }}" type="file" hidden>
+    <label for="imagefile">
+        <input id="imagefile" name="{{ $name }}" type="file" hidden>
         <img {{ $attributes->class('w-100 border border-2 border-primary') }} id="liveImage"
             src="{{ $image ? useImage($image) : asset('images/Placeholder_view_vector.svg.png') }}">
     </label>
