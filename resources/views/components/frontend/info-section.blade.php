ab760afee929cebe506e58b48a247acc71d43546
Mojahidul Islam <bdemon245@gmail.com>
2023-05-01 23:19:55 +0600

    info section done frontend

--- resources/views/components/frontend/info-section.blade.php
+++ resources/views/components/frontend/info-section.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+@php
+    $title = $attributes->get('title');
+@endphp
+<section class="mt-5 mb-2 px-5 py-2">
+    <div>
+        <h2 {{ $attributes->merge(['class' => 'text-uppercase text-center mb-5']) }}
+            style="font-size: 28px; font-weight: 800;">{{ $title }}</h2>
+        <div class="d-flex align-items-center justify-content-center" style="flex-wrap: wrap;">
+            {{ $slot }}
+        </div>
+    </div>
+    <div>
+
+    </div>
+</section>
--- resources/views/frontend/pages/welcome.blade.php
+++ resources/views/frontend/pages/welcome.blade.php
@@ -3,4 +3,16 @@
     <x-frontend.hero-section />
     <x-frontend.products-section />
     <x-frontend.appointment-section />
+    <x-frontend.info-section title="We Help You File Quickly And Confidently" class="">
+        <x-frontend.info-card />
+        <x-frontend.info-card />
+        <x-frontend.info-card />
+        <x-frontend.info-card />
+    </x-frontend.info-section>
+    <x-frontend.info-section title="Lorem ipsum dolor sit amet consectetur" class="text-danger">
+        <x-frontend.info-card />
+        <x-frontend.info-card />
+        <x-frontend.info-card />
+        <x-frontend.info-card />
+    </x-frontend.info-section>
 @endsection
