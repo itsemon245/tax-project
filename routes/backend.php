60a401fb102d03ce3bd9703ae24aa9ca1b727744
Rishad Alam <sazzadullalamrishad@gmail.com>
2023-05-02 23:12:22 +0600

    feat: info section files create

--- routes/backend.php
+++ routes/backend.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Backend\Info\InfoController;
 use App\Http\Controllers\Backend\Hero\BannerController;
 use App\Http\Controllers\Backend\UserProfileController;
 use App\Http\Controllers\Backend\Product\ProductController;
@@ -32,6 +33,7 @@ Route::prefix('admin')->group(function () {
     Route::resource('product-category', ProductCategoryController::class);
     Route::resource('product-subcategory', ProductSubCategoryController::class);
     Route::resource('hero', BannerController::class);
+    Route::resource('info', InfoController::class);
 
     Route::POST('/get-sub-categories/{categoryId}', [ProductController::class, 'getSubCategories'])->name('getSubcategory');
 });
