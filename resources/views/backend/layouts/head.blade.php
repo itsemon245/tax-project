f097b6ea30de61f3ded0b776f1fe0a6033c764e6
Rishad Alam <sazzadullalamrishad@gmail.com>
2023-05-02 11:02:47 +0600

    feat: csrf token Added on the head

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -1,6 +1,7 @@
 <head>
 
     <meta charset="utf-8" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Dashboard | UBold - Responsive Admin Dashboard Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
