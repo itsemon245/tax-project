82b0026ceb0f772c372137e3afd2d08bc1c76853
Mojahidul Islam <bdemon245@gmail.com>
2023-05-06 15:55:29 +0600

    random ui fixes

--- resources/views/components/frontend/hero-section.blade.php
+++ resources/views/components/frontend/hero-section.blade.php
@@ -10,15 +10,17 @@
         <div class="carousel-inner" role="listbox">
             @foreach ($banners as $banner)
                 <div class="carousel-item {{ $banners[0]->id === $banner->id ? 'active' : '' }}">
-                    <div class="row align-items-center" style="position: relative;">
-                        <div id="hero-content-{{ $banner->id }}" class="hero-content"
-                            style="position: absolute;left:15%;max-width:320px;">
-                            <h3 class="mb-0" style="font-weight: 800;font-size:40px;">{{$banner->title}}
-                            </h3>
-                            <p class="my-1" style="font-size:13px;font-weight:500;">{{$banner->sub_title}}</p>
-                            <a href="{{$banner->button}}" class="btn btn-primary text-dark" style="font-weight:600;">
-                                Get Started
-                            </a>
+                    <div class="row" style="position: relative;">
+                        <div class="d-flex align-items-center" style="position: absolute;inset:0;padding-left:15%;">
+                            <div id="hero-content-{{ $banner->id }}" class="hero-content" style="max-width:320px;">
+                                <h3 class="mb-0" style="font-weight: 800;font-size:40px;">{{ $banner->title }}
+                                </h3>
+                                <p class="my-1" style="font-size:13px;font-weight:500;">{{ $banner->sub_title }}</p>
+                                <a href="{{ $banner->button }}" class="btn btn-primary text-dark"
+                                    style="font-weight:600;">
+                                    Get Started
+                                </a>
+                            </div>
                         </div>
                         <img id="{{ $banner->id }}" class="hero-image" src="{{ useImage($banner->image_url) }}"
                             alt="" crossOrigin="anonymous" style="height: 350px; object-fit: cover;"
@@ -40,15 +42,15 @@
 
 @push('customJs')
     <script>
-        const images = $('.hero-image')
-        $.each(images, (index, image) => {
-            image.addEventListener('load', () => {
-                const color = extractColor.getImageColor(image)
-                const textColor = extractColor.getContrastColor(color)
-                console.log(textColor);
-                $(`#hero-content-${image.id}`).css('color', `${textColor}!important`)
-                // console.log( $(`#hero-content-${image.id}`).css('color'));
-            })
-        });
+        // const images = $('.hero-image')
+        // $.each(images, (index, image) => {
+        //     image.addEventListener('load', () => {
+        //         const color = extractColor.getImageColor(image)
+        //         const textColor = extractColor.getContrastColor(color)
+        //         console.log(textColor);
+        //         $(`#hero-content-${image.id}`).css('color', `${textColor}!important`)
+        //         // console.log( $(`#hero-content-${image.id}`).css('color'));
+        //     })
+        // });
     </script>
 @endpush
