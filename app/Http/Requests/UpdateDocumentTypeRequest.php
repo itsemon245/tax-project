3c94c9486ca13c95894d8c54ad354967d62080d0
Mojahidul Islam <bdemon245@gmail.com>
2023-05-14 14:43:41 +0600

    issue: has an issue interacting with docType model

--- app/Http/Requests/UpdateDocumentTypeRequest.php
+++ app/Http/Requests/UpdateDocumentTypeRequest.php
@@ -11,7 +11,7 @@ class UpdateDocumentTypeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- resources/views/backend/userdoc/editDocType.blade.php
+++ resources/views/backend/userdoc/editDocType.blade.php
@@ -2,49 +2,24 @@
 
 
 @section('content')
-  <x-backend.ui.breadcrumbs :list="['Frontend', 'Docment type', 'Edit']" />
+    <x-backend.ui.breadcrumbs :list="['Frontend', 'Docment type', 'Edit']" />
 
-  <x-backend.ui.section-card name="Edit Document Type">
+    <x-backend.ui.section-card name="Edit Document Type">
 
-{{-- add category field --}}
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-body">
-                <h4 class="header-title mb-1">Edit Document Type</h4>
-                <div class="row">
-                    <div class="col-lg-12">
-                        {{ dd($documentType) }}
-                        <form action="" method="POST">
-                            @csrf
-                            {{-- @method('PUT') --}}
-                            <div>
-                                <input type="text" id="simpleinput" value="{{ $documentType->doc_type_name }}" name="add_document_type" placeholder="Update Document Type" class="form-control
-                                @error('add_document_type')
-                                is-invalid
-                                @enderror
-                                ">
-                                @error('add_document_type')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="mt-1">
-                                <x-backend.ui.button class="btn-primary btn-sm w-100">Update Document type</x-backend.ui.button>
-                            </div>
-                        </form>
-                    </div> <!-- end col -->
-                </div>
-            </div> <!-- end card-body -->
-        </div> <!-- end card -->
-    </div><!-- end col -->
-</div>
-      
-  </x-backend.ui.section-card>
-  
+        {{-- add category field --}}
+        <form action="{{route('user-doc-type.update', $documentType->id)}}" method="POST">
+            @csrf
+            @method('PUT')
+            <x-backend.form.text-input name="add_document_type" label="Document Type" :value="$documentType->doc_type_name" />
+            <div class="mt-1">
+                <x-backend.ui.button class="btn-primary btn-sm w-100">Update Document type</x-backend.ui.button>
+            </div>
+        </form>
+        </div>
+    </x-backend.ui.section-card>
 
-  @push('customJs')
-      <script>
-          
-      </script>
-  @endpush
+
+    @push('customJs')
+        <script></script>
+    @endpush
 @endsection
