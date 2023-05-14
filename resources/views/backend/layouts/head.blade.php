dfb37e1a9459db52b2c7d6ee8dc1f20c144667d7
Mojahidul Islam <bdemon245@gmail.com>
2023-05-14 21:35:16 +0600

    fix: moved script links for tables

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -11,18 +11,12 @@
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
 
-    <!-- Plugins css -->
-    <link href="{{ asset('backend/assets/libs/flatpickr/flatpickr.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('backend/assets/libs/selectize/css/selectize.bootstrap3.css') }}" rel="stylesheet"
-        type="text/css" />
-    <link href="{{ asset('backend/assets/libs/fullcalendar/main.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- Bootstrap css -->
     <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- App css -->
     <link href="{{ asset('backend/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
     <!-- icons -->
     <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('backend/assets/css/toastr.min.css') }}">
 
     
     {{-- Plugins link  --}}
--- resources/views/backend/layouts/script.blade.php
+++ resources/views/backend/layouts/script.blade.php
@@ -7,21 +7,7 @@
 
  <script src="{{ asset('backend/assets/libs/selectize/js/standalone/selectize.min.js') }}"></script>
 
- <!-- datatable js start -->
- <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-buttons/js/buttons.flash.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/datatables.net-select/js/dataTables.select.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/pdfmake/build/pdfmake.min.js') }}"></script>
- <script src="{{ asset('backend/assets/libs/pdfmake/build/vfs_fonts.js') }}"></script>
- <!-- datatable js ends -->
+
 
 
 
