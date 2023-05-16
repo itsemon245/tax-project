b33f834a597d7510d8f08d0de6333bb2b7f10b86
Mojahidul Islam <bdemon245@gmail.com>
2023-05-16 12:11:54 +0600

    books page

--- resources/views/frontend/pages/books.blade.php
+++ resources/views/frontend/pages/books.blade.php
@@ -1,4 +1,27 @@
 @extends('frontend.layouts.app')
 @section('main')
-    
+    <section class="py-12 py-sm-24 py-md-32 my-5">
+        <div class="container">
+            <div class="mw-7xl mx-auto">
+                <div class="row">
+                    <div class="col-6 col-md-4 col-lg-3 col-xxl-2 mb-8 mb-lg-0">
+                        <div class="d-flex mw-md mx-auto pb-10 px-10 bg-primary rounded position-relative overflow-hidden"
+                            style="height: 300px;"><img class="position-absolute top-0 start-0 h-100 w-100"
+                                src="https://picsum.photos/seed/random/300" alt="" style="object-fit: cover;" />
+
+                            <div class="position-relative mt-auto p-3 w-100" style='
+                            background: #38bff8b4;  /* fallback for old browsers */
+                            background: -webkit-linear-gradient(to bottom, #acb6e578, #38bff880);  /* Chrome 10-25, Safari 5.1-6 */
+                            background: linear-gradient(to bottom, #acb6e578, #38bff880); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+                            '>
+                                <h4 class="fs-5 mb-1 text-dark">Leslie Alexander</h4><span class="text-dark">Co-Founder,
+                                    CEO</span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
