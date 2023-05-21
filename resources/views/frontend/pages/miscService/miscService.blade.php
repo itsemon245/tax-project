1d99712f8f6aa270517f9f8666128198974aefd3
Mojahidul Islam <bedemon245@gmail.com>
2023-05-21 20:07:04 +0600

    separated industries section into component

--- resources/views/frontend/pages/miscService/miscService.blade.php
+++ resources/views/frontend/pages/miscService/miscService.blade.php
@@ -10,52 +10,7 @@
     <x-frontend.hero-section :banners="$banners" />
 
     {{-- industries section  --}}
-    <div class="py-5 row px-5">
-        <h3 class="text-center">Industries We Serve</h3>
-        <div class="d-flex justify-content-center">
-            <p class="text-justify" style="max-width:1200px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. A numquam inventore consectetur? Dolores sed at atque non repellat consequatur aperiam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab voluptas rerum tenetur dolores officia veritatis reprehenderit blanditiis earum culpa laboriosam.</p>
-        </div>
-        <div class="d-flex justify-content-center">
-            <div class="bg-light rounddd rounded-2 p-3">
-                <div class="industry-grid">
-                    <div>
-                        <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                        <h6>Asset Management</h6>
-                        <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                    </div>
-                    <div>
-                        <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                        <h6>Asset Management</h6>
-                        <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                    </div>
-                    <div>
-                        <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                        <h6>Asset Management</h6>
-                        <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                    </div>
-                    <div>
-                        <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                        <h6>Asset Management</h6>
-                        <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                    </div>
-                    <div>
-                        <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                        <h6>Asset Management</h6>
-                        <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                    </div>
-                    <div>
-                        <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                        <h6>Asset Management</h6>
-                        <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                    </div>
-                   
-                </div>
-                <div class="d-flex justify-content-center">
-                    <a class="btn btn-secondary text-black mt-3" href="#">All Industries</a>
-                </div>
-            </div>
-        </div>
-    </div>
+   <x-frontend.industries-section />
 
 
 
@@ -74,22 +29,5 @@
     <x-frontend.testimonial-section :testimonials="$testimonials">
     </x-frontend.testimonial-section>
    
-@pushOnce('customCss')
-    <style>
-        /* .industry-grid{
-            display: flex;
-            flex-wrap: wrap;
-            gap:1.2rem;
-        }
-        .industry-grid > * {
-            max-width: 32%;
-        } */
-
-        .industry-grid{
-            display: grid;
-            grid-auto-columns: repeat(auto-fit, minmax(300px, 1fr));
-        }
-    </style>
-@endPushOnce
 
 @endsection
