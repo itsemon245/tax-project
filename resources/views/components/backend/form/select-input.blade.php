16cf762e55793a4b0d2bdee26a9c2404886953bc
Mojahidul Islam <bedemon245@gmail.com>
2023-05-26 01:15:23 +0600

    added: selectize js

--- resources/views/components/backend/form/select-input.blade.php
+++ resources/views/components/backend/form/select-input.blade.php
@@ -7,14 +7,6 @@
     $placeholder = $attributes->get('placeholder');
 @endphp
 
-@pushOnce('customCss')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/css/selectize.default.min.css"
-        integrity="sha512-pTaEn+6gF1IeWv3W1+7X7eM60TFu/agjgoHmYhAfLEU8Phuf6JKiiE8YmsNC0aCgQv4192s4Vai8YZ6VNM6vyQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/js/selectize.min.js"
-        integrity="sha512-IOebNkvA/HZjMM7MxL0NYeLYEalloZ8ckak+NDtOViP7oiYzG5vn6WVXyrJDiJPhl4yRdmNAG49iuLmhkUdVsQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-@endPushOnce
 <div class="">
     @if ($label)
         <label for="{{ $id }}" class="form-label">{{ $label }}
