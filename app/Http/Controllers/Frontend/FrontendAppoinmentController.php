8a4d094074423bad3178f5b69a492d3390883a31
Zabir Emu <zabirraihan570@gmail.com>
2023-05-27 16:17:47 +0600

    complete testmonial and expert section

--- app/Http/Controllers/Frontend/FrontendAppoinmentController.php
+++ app/Http/Controllers/Frontend/FrontendAppoinmentController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class FrontendAppoinmentController extends Controller
 {
     public function create()
     {
-        return view('frontend.pages.appoinment.create-appoinment');
+        $testimonials = Testimonial::get();
+        return view('frontend.pages.appoinment.create-appoinment', compact('testimonials'));
     }
 }
--- resources/views/frontend/pages/appoinment/create-appoinment.blade.php
+++ resources/views/frontend/pages/appoinment/create-appoinment.blade.php
@@ -1,3 +1,69 @@
 @extends('frontend.layouts.app')
 @section('main')
+    <x-frontend.hero-section :banners="getRecords('banners')" />
+
+    <div class="app_main">
+        <div id="app_header">
+            <div class="contaciner">
+                <div class="row">
+                    <div class="app_header_content text-center">
+                        <h2>What are your sources of income?</h2>
+                        <p>You may lorme ipsum dellor ahoid jksdfh lksdf</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="app_content">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5 d-flex flex-wrap">
+                        <div class="app_content_thumabil mb-3">
+                            <img src="{{ asset('frontend/assets/images/Rectangle 39.png') }}" alt="">
+                            <h2>Lorem Ipsum</h2>
+                        </div>
+                        <div class="app_content_thumabil mb-3">
+                            <img src="{{ asset('frontend/assets/images/Rectangle 39.png') }}" alt="">
+                            <h2>Lorem Ipsum</h2>
+                        </div>
+                        <div class="app_content_thumabil mb-3">
+                            <img src="{{ asset('frontend/assets/images/Rectangle 39.png') }}" alt="">
+                            <h2>Lorem Ipsum</h2>
+                        </div>
+                        <div class="app_content_thumabil mb-3">
+                            <img src="{{ asset('frontend/assets/images/Rectangle 39.png') }}" alt="">
+                            <h2>Lorem Ipsum</h2>
+                        </div>
+                        <div class="app_content_thumabil mb-3">
+                            <img src="{{ asset('frontend/assets/images/Rectangle 39.png') }}" alt="">
+                            <h2>Lorem Ipsum</h2>
+                        </div>
+                        <div class="app_content_thumabil mb-3">
+                            <img src="{{ asset('frontend/assets/images/Rectangle 39.png') }}" alt="">
+                            <h2>Lorem Ipsum</h2>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 d-flex flex-column align-items-center justify-content-center">
+                        <div class="app_icon">
+                            <img src="{{ asset('frontend/assets/images/Frame.png') }}" alt="" class="icon_thumbail">
+                            <h2>Professional</h2>
+                        </div>
+                        <div class="app_icon">
+                            <img src="{{ asset('frontend/assets/images/Frame (2).png') }}" alt=""
+                                class="icon_thumbail">
+                            <h2>Professional</h2>
+                        </div>
+                        <div class="app_icon">
+                            <img src="{{ asset('frontend/assets/images/Frame (1).png') }}" alt=""
+                                class="icon_thumbail">
+                            <h2>Professional</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <x-frontend.testimonial-section :$testimonials>
+    </x-frontend.testimonial-section>
 @endsection
