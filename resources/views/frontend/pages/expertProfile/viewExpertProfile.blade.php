9236910388f3a7bca4bfdc418ce42290b75399d1
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2023-05-31 21:04:09 +0600

    feat: Expert Profile completed

--- resources/views/frontend/pages/expertProfile/viewExpertProfile.blade.php
+++ resources/views/frontend/pages/expertProfile/viewExpertProfile.blade.php
@@ -62,6 +62,63 @@
                 <a href="#" class="me-3">Reviews</a>
             </div>
         </div>
+
+        <div class="container my-3">
+            <div class="row">
+                <div class="col-lg-6 p-0 mb-3 mb-lg-0">
+                    <div class="shadow p-3 rounded-3 bg-white">
+                        <h4><b>Dr. Ekramul Haque - MBBS</b></h4>
+                        <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quisquam
+                            illum odit totam velit sit, aliquam beatae ab porro architecto, hic recusandae sed. Delectus
+                            quas sit, eveniet ratione possimus beatae praesentium odit reiciendis quia aperiam placeat vero
+                            doloribus maxime, magni, commodi necessitatibus dolore. Quaerat, beatae vitae eum non
+                            voluptatibus consequatur saepe ut, iure enim provident porro molestiae maxime perspiciatis
+                            debitis, nobis tenetur exercitationem obcaecati iusto autem illum. Adipisci at enim assumenda
+                            officiis provident aliquam iste delectus dolores, vitae quidem, nobis fugiat vel. Dolores
+                            excepturi laboriosam eum expedita ut labore accusantium quaerat quibusdam in sit autem eius,
+                            assumenda, aspernatur, ipsum quas!</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 p-0 ps-lg-3">
+                    <div class="shadow p-3 rounded-3 bg-white mb-3">
+                        <h4><b>Availability</b></h4>
+                        <div class="border-start border-3 ps-2">
+                            <p class="text-muted m-0">Instant Consultation time</p>
+                            <p><b>Sat-Fri(08:00AM-11:55PM)</b></p>
+                        </div>
+                    </div>
+                    <div class="shadow p-3 rounded-3 bg-white">
+                        <h4><b>At a Glance</b></h4>
+                        <div class="row mt-3">
+                            <div class="col-md-6 mb-3">
+                                <div class="border-start border-3 ps-2">
+                                    <p class="text-muted m-0">Instant Consultation time</p>
+                                    <p><b>Sat-Fri(08:00AM-11:55PM)</b></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <div class="border-start border-3 ps-2">
+                                    <p class="text-muted m-0">Instant Consultation time</p>
+                                    <p><b>Sat-Fri(08:00AM-11:55PM)</b></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="border-start border-3 ps-2">
+                                    <p class="text-muted m-0">Instant Consultation time</p>
+                                    <p><b>Sat-Fri(08:00AM-11:55PM)</b></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="border-start border-3 ps-2">
+                                    <p class="text-muted m-0">Instant Consultation time</p>
+                                    <p><b>Sat-Fri(08:00AM-11:55PM)</b></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
 
