af182fccfccab32fe8af3a4979077232e2cc97ed
Zabir Emu <zabirraihan570@gmail.com>
2023-06-02 12:26:45 +0600

    complete header part

--- resources/views/frontend/pages/browseTaxExpert/index.blade.php
+++ resources/views/frontend/pages/browseTaxExpert/index.blade.php
@@ -5,4 +5,22 @@
         $banners = getRecords('banners');
     @endphp
     <x-frontend.hero-section :banners="$banners" />
+
+    <div id="browse_section">
+        <div class="container">
+            <div class="d-flex align-items-center">
+                <h2 class="browse_header">Browse Experts</h2>
+
+                <hr class="line" style="width:80%">
+                <div class="circle_left"></div>
+                <div class="circle_right"></div>
+            </div>
+            <div class="row">
+
+                <div class="col-lg-12">
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
