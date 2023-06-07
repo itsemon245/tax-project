8bf08d688de703832f47c557e305afa2d7f3522e
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2023-06-07 21:23:03 +0600

    feat: create service complete

--- app/Http/Requests/StoreServiceRequest.php
+++ app/Http/Requests/StoreServiceRequest.php
@@ -11,7 +11,7 @@ class StoreServiceRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class StoreServiceRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "title" => 'required|max:100',
+            "intro" => 'required|max:100',
+            "ratting" => 'required|max:1',
+            "reviews" => 'required|max:100'
         ];
     }
 }
