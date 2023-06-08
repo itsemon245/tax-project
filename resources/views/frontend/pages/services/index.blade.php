dedd6bbb44c222ce15421ed68fc305334355d698
Mojahidul Islam <bdemon245@gmail.com>
2023-06-08 13:18:35 +0600

    fix: services responsivness

--- resources/views/frontend/pages/services/index.blade.php
+++ resources/views/frontend/pages/services/index.blade.php
@@ -6,15 +6,28 @@
     $banners = getRecords('banners');
 @endphp
 @extends('frontend.layouts.app')
+
+@pushOnce('customCss')
+    <style>
+        .custom-grid {
+            display: grid;
+            max-width: 100%;
+            grid-template-columns: repeat(auto-fit, minmax(205px, 1fr) );
+            justify-content: center;
+            align-items: center;
+        }
+        
+    </style>
+@endPushOnce
 @section('main')
     <x-frontend.hero-section :banners="$banners" />
 
         {{-- Services --}}
     <section class="px-lg-5 px-2 my-5">
         <h4 class="text-center my-5" style="font-size:28px; font-weight:600;">{{$services[0]->serviceSubCategory->name}}</h4>
-        <div class="row mx-lg-5 mx-2">
+        <div class="custom-grid px-3 gap-3">
             @foreach ($services as $service)
-            <div class="col-md-4 col-lg-3 col-sm-6 mb-3">
+            <div class="">
                 <div class="d-flex flex-column align-items-center border">
                     <img style="width:150px;aspect-ratio:1/1;" class="rounded rounded-circle mb-3"
                         src="{{ useImage($service->image) }}" alt="">
@@ -25,7 +38,7 @@
                     
                 </div>
                 <div class="bg-primary px-3 d-flex align-items-center justify-content-between">
-                    <div class="d-inline-flex gap-2">
+                    <div class="d-inline-flex flex-column">
                         <div class="d-inline-flex">
                             <span class="mdi mdi-star" style="color: yellow;"></span>
                             <span class="mdi mdi-star" style="color: yellow;"></span>
@@ -34,12 +47,16 @@
                             <span class="mdi mdi-star" style="color: yellow;"></span>
                         </div>
 
-                        <div class="d-inline-flex align-items-center">
+                        <div class="d-inline-flex align-items-center" style="margin-top: -0.8rem;">
                             <span class="mdi mdi-account"></span>
                             <span>{{$service->reviews}}</span>
                         </div>
                     </div>
-                    <span style="font-weight: 500;"> Tk. {{$service->price}}/-</span>
+                    <div class="d-inline-flex flex-column" style="margin-top: -0.5rem;">
+                        <strong>Price </strong>
+                        <span style="font-weight: 500;">{{$service->price}} Tk</span>
+                    </div>
+                    
                 </div>
             </div>
             @endforeach
