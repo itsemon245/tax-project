d23835ac0118d741d64be4c82dc255a8116994a3
Zabir Emu <zabirraihan570@gmail.com>
2023-06-23 16:28:04 +0600

    fetch about data

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -1,5 +1,5 @@
 @php
-    $banners =getRecords('banners');
+    $banners = getRecords('banners');
     $partners = getRecords('partner_sections');
 @endphp
 @extends('frontend.layouts.app')
@@ -12,13 +12,7 @@
             <h3 class="text-center my-3">About Us</h3>
             <div class="">
                 <div class="bg-secondary p-3 rounded">
-                    <p class="d-flex justify-content-left p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Odit repudiandae, nisi omnis fuga quis expedita, culpa ad beatae mollitia maxime commodi non? Excepturi
-                        omnis laborum voluptatem? Nobis adipisci eum eius ipsam deleniti ipsa delectus qui! Lorem ipsum dolor
-                        sit amet consectetur adipisicing elit. Beatae optio dignissimos natus molestiae ut dolorum cum quaerat
-                        ratione dolores soluta. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus maxime
-                        iusto dolore cumque repudiandae aperiam aspernatur. Eveniet deserunt nobis sit aspernatur incidunt
-                        tenetur, reiciendis numquam iusto. Velit perspiciatis neque ipsam.</p>
+                    <p class="d-flex justify-content-left p-3">{!! $about_data->description ?? ' ' !!}</p>
                 </div>
             </div>
         </section>
@@ -31,43 +25,28 @@
             <div class="">
                 {{-- Left side content --}}
                 <div class="row justify-content-center">
-                    {{-- Section One --}}
-                    <div class="row mb-4 justify-content-sm-center gap-sm-3">
-                        <h4 class="col-sm-12 col-5 p-0">Recurment Documents</h4>
-                        <div class="col-sm-3 col-6 mb-3 mb-sm-0 p-0 flex-grow-1">
-                            <img class="w-100 rounded" src="{{ asset('frontend/assets/images/bg-material.png') }}" alt=""/>
-                        </div>
-                        <div class="col-sm-8 p-4 bg-secondary rounded">
-                            <p>
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                            </p>
-                        </div>
-                    </div>
 
-                        {{-- Section two --}}
-                    <div class="row mb-4 justify-content-sm-center gap-sm-3">
-                    <h4 class="col-sm-12 col-5 p-0">Recurment Documents</h4>
-                    <div class="col-sm-3 col-6 mb-3 mb-sm-0 p-0 flex-grow-1">
-                        <img class="w-100 rounded" src="{{ asset('frontend/assets/images/bg-material.png') }}" alt=""/>
-                    </div>
-                    <div class="col-sm-8 p-4 bg-secondary rounded">
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt in modi aperiam ducimus, possimus tempore eaque nulla iure impedit ut.
-                        </p>
-                    </div>
-                    </div>
+                    @foreach ($about_sections as $about_section)
+                        {{-- Sections --}}
+                        <div class="row mb-4 justify-content-sm-center gap-sm-3">
+                            <h4 class="col-sm-12 col-5 p-0">{!!$about_sections[0]['title'] ?? '' !!}</h4>
+                            <div class="col-sm-3 col-6 mb-3 mb-sm-0 p-0 flex-grow-1">
+                                <img class="w-100 rounded"
+                                    src="{{ $about_sections[0]['image'] ? useImage($about_sections[0]['image']) : asset('frontend/assets/images/bg-material.png') }}"
+                                    alt="" />
+                            </div>
+                            <div class="col-sm-8 p-4 bg-secondary rounded">
+                                <p>
+                                    {!! $about_sections[0]['description'] ?? '' !!}
+                                </p>
+                            </div>
+                        </div>
+                    @endforeach
+
                 </div>
             </div>
         </div>
-        
+
     </div>
     <section class="mt-5 py-5" style="background: #474646;">
         <h3 class="text-light text-center">Our Valuable Partners</h3>
@@ -77,40 +56,41 @@
 
                 {{-- Patner section is starting --}}
                 @foreach ($partners as $partner)
-                <div class="media-elements">
-                    <div class="d-flex align-items-center gap-3 p-3" style="width: 100%;">
-                        <div>
-                            <img class="border image rounded-circle"
-                                src="{{useImage($partner->image) ==! null ? 'useImage($partner->image)' : 'https://api.dicebear.com/6.x/bottts/png' }}" alt="">
+                    <div class="media-elements">
+                        <div class="d-flex align-items-center gap-3 p-3" style="width: 100%;">
+                            <div>
+                                <img class="border image rounded-circle"
+                                    src="{{ useImage($partner->image) == !null ? 'useImage($partner->image)' : 'https://api.dicebear.com/6.x/bottts/png' }}"
+                                    alt="">
 
-                        </div>
-                        <div>
-                            <h3 class="mb-0">{{ $partner->name }}</h3>
-                            <small class="mb-0 text-muted">{{ $partner->designation }}</small>
-                            <div class="d-flex mb-0 mt-2 text-primary">
-                               <a href="mailto:{{ $partner->email }}">
-                                <p class="me-2 mb-0"><i class="fe-mail"></i></p>
-                                <p class="mb-0">{{ $partner->email }}</p>
-                               </a>
                             </div>
-                            <div class="d-flex text-primary">
-                               <p class="me-2 mb-0"> <i class="fe-phone"></i></p>
-                                <p class="mb-0">{{ $partner->phone }}</p>
-                            </div>
-                            <div class="d-flex mt-3 text-primary">
-                                <a href="{{ $partner->facebook }}">
-                                    <i class="fe-facebook me-3"></i>
-                                </a>
-                                <a href="{{ $partner->twitter }}">
-                                    <i class="fe-twitter me-3"></i>
-                                </a>
-                                <a href="{{ $partner->linkedin }}">
-                                    <i class="fe-linkedin me-3"></i>
-                                </a>
+                            <div>
+                                <h3 class="mb-0">{{ $partner->name }}</h3>
+                                <small class="mb-0 text-muted">{{ $partner->designation }}</small>
+                                <div class="d-flex mb-0 mt-2 text-primary">
+                                    <a href="mailto:{{ $partner->email }}">
+                                        <p class="me-2 mb-0"><i class="fe-mail"></i></p>
+                                        <p class="mb-0">{{ $partner->email }}</p>
+                                    </a>
+                                </div>
+                                <div class="d-flex text-primary">
+                                    <p class="me-2 mb-0"> <i class="fe-phone"></i></p>
+                                    <p class="mb-0">{{ $partner->phone }}</p>
+                                </div>
+                                <div class="d-flex mt-3 text-primary">
+                                    <a href="{{ $partner->facebook }}">
+                                        <i class="fe-facebook me-3"></i>
+                                    </a>
+                                    <a href="{{ $partner->twitter }}">
+                                        <i class="fe-twitter me-3"></i>
+                                    </a>
+                                    <a href="{{ $partner->linkedin }}">
+                                        <i class="fe-linkedin me-3"></i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
 
             </div>
