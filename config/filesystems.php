007985763095928b375cada6da592dd02758fe65
Mojahidul Islam <bdemon245@gmail.com>
2023-07-01 02:52:51 +0600

    added new disk to local stroage named temp

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,6 +43,13 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        'temp' => [
+            'driver' => 'local',
+            'root' => storage_path('app/temp'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
 
         's3' => [
             'driver' => 's3',
