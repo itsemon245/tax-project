a3b00d8693fcd0f18a41431969aef7f806ad405b
Mojahidul Islam <bdemon245@gmail.com>
2023-07-02 23:37:16 +0600

    test mac 2

--- routes/frontend.php
+++ routes/frontend.php
@@ -89,7 +89,7 @@ Route::post('/upload', function (Request $request) {
 });
 
 Route::get('get-mac', function(){
-    dd(shell_exec('netstat -ie'));
+    dd(shell_exec('echo "Hello World"'));
 });
 Route::get('get-ip', function(Request $request){
     dd($request->ip());
