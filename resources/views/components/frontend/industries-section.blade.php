8b428a8d2a9dabe17f1044357eab3b9ea599dd4b
Mojahidul Islam <bdemon245@gmail.com>
2023-07-08 21:07:42 +0600

    retouching: industries page

--- resources/views/components/frontend/industries-section.blade.php
+++ resources/views/components/frontend/industries-section.blade.php
@@ -1,36 +1,31 @@
-<section class="py-5 row px-lg-5 px-2">
+<section class="row px-lg-5 px-2 mt-5">
     <h3 class="text-center">Industries We Serve</h3>
     <div class="d-flex justify-content-center">
-        <p class="text-justify" style="max-width:1000px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. A numquam inventore consectetur? Dolores sed at atque non repellat consequatur aperiam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab voluptas rerum tenetur dolores officia veritatis reprehenderit blanditiis earum culpa laboriosam.</p>
-    </div>
-    <div class="d-flex justify-content-center">
-        <div class="bg-light rounded-3 p-3 mx-lg-5 mx-2">
+        <div class="container">
             <div class="row">
-                <div class="col-md-4 col-sm-6">
-                    <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                    <h6>Asset Management</h6>
-                    <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
+                <div class="container">
+                    <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+                        numquam inventore consectetur? Dolores sed at atque non repellat consequatur aperiam. Lorem
+                        ipsum dolor, sit
+                        amet consectetur adipisicing elit. Ab voluptas rerum tenetur dolores officia veritatis
+                        reprehenderit
+                        blanditiis earum culpa laboriosam.</p>
                 </div>
-                <div class="col-md-4 col-sm-6">
-                    <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                    <h6>Asset Management</h6>
-                    <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                    <h6>Asset Management</h6>
-                    <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                    <h6>Asset Management</h6>
-                    <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img style="width:48px;" src="{{asset('backend/assets/images/products/product-6.png')}}" alt="">
-                    <h6>Asset Management</h6>
-                    <p  class="tex-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, repellendus?</p>
-                </div>
+                @foreach (range(1, 6) as $key)
+                    <div class="col-md-4 col-sm-6 mb-3">
+                        <div class="border bg-light w-100 px-0 px-md-3 px-lg-5 py-3 rounded">
+                            <a class="text-dark">
+                                <img style="width:48px;"
+                                    src="{{ asset('backend/assets/images/products/product-6.png') }}" alt="" />
+                                <h6>Asset Management</h6>
+                                <p class="tex-justify text-muted">Lorem ipsum dolor, sit amet consectetur adipisicing
+                                    elit.
+                                    Veniam, repellendus?</p>
+                            </a>
+                        </div>
+                    </div>
+                @endforeach
+
             </div>
             <div class="d-flex justify-content-center">
                 <a class="btn btn-secondary text-black mt-3" href="#">All Industries</a>
