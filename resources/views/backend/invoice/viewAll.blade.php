30a432f14ad1deb905278f9acb9da2093387a7ae
Mojahidul Islam <bdemon245@gmail.com>
2023-07-17 13:52:24 +0600

    recent cards in invoice

--- resources/views/backend/invoice/viewAll.blade.php
+++ resources/views/backend/invoice/viewAll.blade.php
@@ -4,7 +4,34 @@
 @section('content')
     <x-backend.ui.breadcrumbs :list="['Management', 'Invoice', 'List']" />
 
-    <x-backend.ui.section-card name="Invoice List">
+    <x-backend.ui.section-card name="Invoices">
+        <div class="row mb-5">
+            <div class="col-md-4 col-lg-3 col-xxl-2 mb-2">
+                <div class="card h-100 shadow" style="border: medium dashed var(--ct-gray-400);">
+                    <div class="card-body h-100">
+                        <div class="d-flex flex-column align-items-center justify-content-center h-100">
+                            <span class="text-success fw-bold display-5">+</span>
+                            <span class="fw-bold text-muted fs-4">New Invoice</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @foreach (range(1, 5) as $item)
+                <div class="col-md-4 col-lg-3 col-xxl-2 mb-2">
+                    <div class="card h-100 shadow border border-2">
+                        <div class="card-body">
+                            <p>0001</p>
+                            <h6>Company Name</h6>
+                            <p class='text-muted mb-0'>Issued: 1 Jul, 2023</p>
+                            <p class='text-muted mb-0'>Due: 1 Agust, 2023</p>
+                        </div>
+                        <div class="bg-soft-success text-success w-100 p-1 text-center fw-bold" style="overflow:hidden;">
+                            Sent
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
         <div class="row">
             <div class="col-12">
                 <div class="card">
@@ -31,12 +58,13 @@
                                         <td>{{ Carbon\Carbon::parse($invoice->issue_date)->format('d M Y') }}</td>
                                         <td>{{ Carbon\Carbon::parse($invoice->due_date)->format('d M Y') }}</td>
                                         <td>
-                                            <span class="fw-bold">{{ $invoice->amount_due. " Tk" }}</span>
+                                            <span class="fw-bold">{{ $invoice->amount_due . ' Tk' }}</span>
                                         </td>
                                         <td>{{ $invoice->status }}</td>
                                         <td>
-                                            <x-backend.ui.button type="custom" href="{{ route('invoice.show', $invoice->id) }}"
-                                                class="btn-sm btn-dark">View</x-backend.ui.button>
+                                            <x-backend.ui.button type="custom"
+                                                href="{{ route('invoice.show', $invoice->id) }}" class="btn-sm btn-dark">
+                                                View</x-backend.ui.button>
                                             <x-backend.ui.button type="delete"
                                                 action="{{ route('invoice.destroy', $invoice->id) }}" class="btn-sm" />
                                         </td>
