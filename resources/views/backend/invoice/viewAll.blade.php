86f839fe95a3ba3887814523c0eeba7d25b65613
Mojahidul Islam <bdemon245@gmail.com>
2023-07-17 16:47:44 +0600

    advance search btn in invoice

--- resources/views/backend/invoice/viewAll.blade.php
+++ resources/views/backend/invoice/viewAll.blade.php
@@ -5,8 +5,21 @@
     <x-backend.ui.breadcrumbs :list="['Management', 'Invoice', 'List']" />
 
     <x-backend.ui.section-card name="Invoices">
-        <div class="row mb-5">
-            <div class="col-md-4 col-lg-3 col-xxl-2 mb-2">
+
+        <div id="advance-search" class="d-inline-block d-none"
+            style="border-radius: 0 25px 25px 0; border: 2px solid var(--ct-gray-500);padding:3.5px 8px;" role="button">
+            <span class="mdi mdi-filter-variant fs-5"></span> Advanced Search <span class="mdi mdi-chevron-down fs-5"></span>
+        </div>
+        {{-- <div id="advance-search-options" class="card" style="border: 2px solid var(--ct-gray-500);">
+            <div class="card-body">
+                <label for=""></label>
+                <input type="checkbox" name="query" id="">
+            </div>
+        </div> --}}
+        <h5 class="p-3">Recently Updated</h5>
+        <div class="d-none d-sm-flex flex-wrap justify-content-center gap-3 mb-5"
+            style="height:220px; overflow-x: hidden; overflow-y:hidden;">
+            <a href="{{ route('invoice.create') }}" class="" style="width: clamp(160px, 180px, 200px);">
                 <div class="card h-100 shadow" style="border: medium dashed var(--ct-gray-400);">
                     <div class="card-body h-100">
                         <div class="d-flex flex-column align-items-center justify-content-center h-100">
@@ -15,9 +28,9 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            @foreach (range(1, 5) as $item)
-                <div class="col-md-4 col-lg-3 col-xxl-2 mb-2">
+            </a>
+            @foreach (range(1, 8) as $item)
+                <div class="" style="width: clamp(160px, 180px, 200px);">
                     <div class="card h-100 shadow border border-2">
                         <div class="card-body">
                             <p>0001</p>
@@ -35,6 +48,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
+                    <h5 class="card-header bg-soft-light text-dark">All Invoices</h5>
                     <div class="card-body">
 
                         <x-backend.table.basic>
@@ -77,10 +91,46 @@
                 </div> <!-- end card -->
             </div><!-- end col-->
         </div>
+
     </x-backend.ui.section-card>
 
     <!-- end row-->
 
     @push('customJs')
+        <script>
+            $(document).ready(function() {
+                const filterWrapper = $('#basic-datatable_filter')
+                // removes serach text from label
+                $('#basic-datatable_filter label').contents().filter(function() {
+                    return this.nodeType === 3;
+                }).remove();
+                const search = $('#basic-datatable_filter input[type="search"]')
+                const searchLabel = search.parent()
+                const advanceSearch = $('#advance-search')
+
+                search
+                    .removeClass('form-control form-control-sm')
+                    .css({
+                        'border': 'none',
+                        'outline': 'transparent',
+                        'padding': 0,
+                        'margin-left': 8
+                    })
+                    .attr('placeholder', 'Search')
+                searchLabel
+                    .prepend('<span class="fas fa-search"></span>')
+                    .css({
+                        'border': '2px solid var(--ct-gray-500)',
+                        'border-radius': '50px 0 0 50px',
+                        'padding': '4px 1rem',
+                        'display': 'inline-flex',
+                        'align-items': 'baseline',
+                    })
+
+
+                console.log(searchLabel);
+                filterWrapper.append(advanceSearch.removeClass('d-none'))
+            });
+        </script>
     @endpush
 @endsection
