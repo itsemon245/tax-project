a559f0a1c7cfd3a9a1e35ff8bb1f58561c42c657
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2023-07-25 22:14:28 +0600

    feat: Migrations for Exam and MCQ test

---
+++ database/migrations/2023_07_25_153324_create_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('exam_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->string('name');
+            $table->decimal('mark');
+            $table->json('choices');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
