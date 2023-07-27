7d5019ad63d6d5de98093d847ddc3eccaf32ac78
mdparvej123 <pj.parvaz45@gmail.com>
2023-07-27 22:08:55 +0600

    feat: client side promo code

--- resources/views/frontend/pages/promoCodesPage.blade.php
+++ resources/views/frontend/pages/promoCodesPage.blade.php
@@ -3,8 +3,8 @@
     <div class="container">
         <h2 class="mt-2 mb-2 p-3">All Promo Codes Here: </h2>
         <div class="row mt-3">
-            @foreach ($user->promoCodes as $code)
-            <div class="col-md-4 {{ $code->status !== 1 ? 'd-none' : '' }}">
+            @foreach ($promoCodes as $code)
+            <div class="col-md-4">
                 <div class="card shadow-sm">
                     <div class="card-body">
                         <div class="d-felx">
@@ -12,7 +12,7 @@
                         </div>
                         <div class="row">
                             <div class="col-md-3">
-                                <h1 class="d-flex justify-content-center mb-0">{{ $code->amount }} <span class="text-success fs-3">{!! $code->is_discount !== 1 ? '&#2547;' : '%' !!}</span> </h1>
+                                <h1 class="d-flex justify-content-center mb-0">{{ $code->amount }} <span class="text-success fs-3">{!! $code->is_discount ? '%' : '&#2547;' !!}</span> </h1>
                             </div>
                             <div class="col-md-9">
                                 <p class="d-flex justify-content-start mt-0 mb-0">Expire : <span class="text-muted"> {{ \Carbon\Carbon::parse( $code->expired_at)->format('d-M-Y') }}</span></p>
@@ -22,7 +22,7 @@
                     </div>
                     <span style="color: var(--bs-gray-500);position: absolute;top:0;right:0;">
                         <div class=" text-center ">
-                            <button class="btn" id="copyButton{{ $code->id }}"><span class="mdi mdi-content-copy"></span></button>
+                            <span class="copyButton mdi mdi-content-copy" data-code="{{ $code->code }}"></span>
                         </div>
                     </span>
                 </div>
@@ -33,17 +33,16 @@
 @endsection
 @push('customJs')
 <script>
-    document.getElementById('copyButton{{ $code->id }}').addEventListener('click', function() {
-        var textToCopy = "{{ $code->code }}"; 
-        var tempInput = document.createElement("input");
-        tempInput.type = "text";
-        tempInput.value = textToCopy;
-        document.body.appendChild(tempInput);
-        tempInput.select();
-        document.execCommand("copy");
-            document.body.removeChild(tempInput);
-            alert("Copied Promo : " + textToCopy);
+    $( ".copyButton" ).each( (i,btn)=> {
+        btn.addEventListener('click', function(e) {
+        var textToCopy = e.target.dataset.code; 
+        navigator.clipboard.writeText(textToCopy);
+        Toast.fire({
+            title: 'Copied', text: 'Copy to Clipboard', icon: 'success'
+        })
         }); 
+    } )
+    
     </script>
 @endpush
 
