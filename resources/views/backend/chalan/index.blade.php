4b953d0e056f868e804c40241e1b0340afaaaa62
Mojahidul Islam <bdemon245@gmail.com>
2023-07-31 23:24:04 +0600

    created box input

--- resources/views/backend/chalan/index.blade.php
+++ resources/views/backend/chalan/index.blade.php
@@ -1,10 +1,12 @@
 @extends('backend.layouts.app')
+
+
 @section('content')
     <x-backend.ui.breadcrumbs :list="['Management', 'Chalan', 'List']" />
 
     <x-backend.ui.section-card name="List Chalan">
 
         <x-backend.form.text-input type="text" name="text_input" class="dotted-border" required />
+        <x-backend.form.box-input value="12548" name="box_input" />
     </x-backend.ui.section-card>
-    <!-- end row-->
 @endsection
