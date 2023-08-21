0607fed457c35ef86dbcb88c378db2d715c7698d
Mojahidul Islam <bdemon245@gmail.com>
2023-08-22 02:12:31 +0600

    user docs ui updated

--- app/Models/FiscalYear.php
+++ app/Models/FiscalYear.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FiscalYear extends Model
 {
@@ -24,5 +25,10 @@ class FiscalYear extends Model
                 'due_date',
                 'status'
             ]);
+    }
+
+    function userDocs(): HasMany
+    {
+        return $this->hasMany(UserDoc::class);
     }
 }
