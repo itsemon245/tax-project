44673ef17a1a00716117636fa08092110590ffaf
Mojahidul Islam <bdemon245@gmail.com>
2023-08-23 22:19:29 +0600

    cast for industries

--- app/Models/Industry.php
+++ app/Models/Industry.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Casts\Json;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Industry extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    protected $casts = [
+        'section' => Json::class,
+    ];
 }
