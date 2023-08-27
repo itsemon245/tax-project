246186c20d50b8f6433247eec5c898834be4c07e
Mojahidul Islam <bdemon245@gmail.com>
2023-08-28 01:17:03 +0600

    fix: filter menu interaction

--- resources/views/components/avg-review-stars.blade.php
+++ resources/views/components/avg-review-stars.blade.php
@@ -6,20 +6,23 @@
     
 @endphp
 <div>
-    <span class="text-dark {{ $class }}">{{ round($avg, 1) }}</span>
     @php
         $color = null;
     @endphp
-    @foreach (range(1, 4) as $rating)
-        @php
-            $color = $rating > $avg ? 'var(--bs-gray-400)' : 'var(--bs-yellow)';
-            
-        @endphp
-        <span class="fas fa-star" style="color: {{ $color }};"></span>
-    @endforeach
-    <div class="d-inline-flex justify-content-center align-items-center position-relative ">
-        <span class="fas fa-star" style="color: var(--bs-gray-400);position:absolute; top:0;left:0;"></span>
-        <span class="fas fa-star" style="z-index:2;color: {{ $color }};{{ starStyle($rating, $avg) }}"></span>
+    <div class="d-flex justify-content-center gap-1 flex-wrap">
+        @foreach (range(1, 4) as $rating)
+            @php
+                $color = $rating > $avg ? 'var(--bs-gray-400)' : 'var(--bs-yellow)';
+                
+            @endphp
+            <span class="fas fa-star {{ $iconFont }}" style="color: {{ $color }};"></span>
+        @endforeach
+        <div class="d-inline-flex justify-content-center align-items-center position-relative ">
+            <span class="fas fa-star {{ $iconFont }}"
+                style="color: var(--bs-gray-400);position:absolute; top:0;left:0;"></span>
+            <span class="fas fa-star {{ $iconFont }}"
+                style="z-index:2;color: {{ $color }};{{ starStyle($rating, $avg) }}"></span>
+        </div>
     </div>
 
 </div>
