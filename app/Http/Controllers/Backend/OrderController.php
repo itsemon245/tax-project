49068fcd287625fe0fca72ad0f6feb7bf2ca7642
Zabir Emu <zabirraihan570@gmail.com>
2023-08-30 23:59:29 +0600

    complele expert approved part

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\ExpertProfile;
 use App\Models\Purchase;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,33 @@ class OrderController extends Controller
         return view('backend.payment.approved', compact('payments'));
     }
 
+    public function consultancyIndex()
+    {
+        $payments = Purchase::with('purchasable')->where('purchasable_type','ExpertProfile')->latest()->get();
+        //dd($payments);
+        return view('backend.payment.consultancyApproved', compact('payments'));
+    }
+
     public function status($id)
+    {
+        $payments = Purchase::find($id);
+        if ($payments->approved === 0) {
+            $payments->approved = 1;
+        } else {
+            $payments->approved = 0;
+        }
+        $payments->save();
+        $notification = [
+            'message' => 'Order Approved Successfully',
+            'alert-type' => 'success',
+        ];
+
+        return redirect()
+            ->back()
+            ->with($notification);
+    }
+
+    public function consultancyStatus($id)
     {
         $payments = Purchase::find($id);
         if ($payments->approved === 0) {
