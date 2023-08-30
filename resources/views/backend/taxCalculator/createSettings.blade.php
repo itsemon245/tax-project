b5801c2893df8f97c047334f6716bd0e14c5400d
Mojahidul Islam <bdemon245@gmail.com>
2023-08-30 21:39:07 +0600

    create Tax Setting Done

--- resources/views/backend/taxCalculator/createSettings.blade.php
+++ resources/views/backend/taxCalculator/createSettings.blade.php
@@ -50,8 +50,8 @@
                                     <label class="form-label mb-0 p-0 col-12">Amount</label>
                                     <div class="d-flex align-items-center justify-content-center border shadow-sm rounded"
                                         style="overflow: hidden;">
-                                        <input type="checkbox" checked id="is-discount" name="is_discounts[]" hidden
-                                            disabled>
+                                        <input type="text" id="is-discount" name="is_discounts_fixed[]" value="false"
+                                            hidden disabled>
                                         <input type="number" id="discount-amount" name="discount_amounts[]"
                                             class="amount border-0 rounded-0 w-100 ps-2" style="outline:transparent;"
                                             placeholder="0" aria-label="Discont" disabled>
@@ -81,11 +81,11 @@
                     <h4 class="text-center fw-bold">Tax Informations</h4>
                     <div class="row">
                         <div class="col-md-3">
-                            <x-backend.form.text-input name="name" placeholder="Name" label="Name" />
+                            <x-backend.form.text-input name="name" placeholder="Name" label="Name" required />
                         </div>
                         <div class="col-md-3">
                             <x-backend.form.select-input name="for" id="for" label="Tax For"
-                                placeholder="Select an option">
+                                placeholder="Select an option" required>
                                 <option value="individual" selected>Individual</option>
                                 <option value="firm">Firm</option>
                                 <option value="company">Company</option>
@@ -93,28 +93,29 @@
                         </div>
                         <div class="col-md-3">
                             <x-backend.form.select-input name="type" id="type" label="Tax Type"
-                                placeholder="Select an option">
+                                placeholder="Select an option" required>
                                 <option value="tax" selected>Tax</option>
                                 <option value="others">Others</option>
                             </x-backend.form.select-input>
                         </div>
                         <div class="col-md-3">
                             <x-backend.form.text-input type="number" name="min_tax" placeholder="Min. Tax"
-                                label="Min. Tax" />
+                                label="Min. Tax" required />
                         </div>
                     </div>
-                    <div class="row only-company d-none">
+                    <div class="row only-company only-tax d-none">
                         <div class="col-md-4">
                             <x-backend.form.text-input type='number' name="turnover_percentage"
-                                placeholder="Percentage For Turnover" label="Percentage For Turnover(%)" disabled />
+                                placeholder="Percentage For Turnover" label="Percentage For Turnover(%)" required
+                                disabled />
                         </div>
                         <div class="col-md-4">
                             <x-backend.form.text-input type='number' name="income_percentage"
-                                placeholder="Percentage For Income" label="Percentage For Income(%)" disabled />
+                                placeholder="Percentage For Income" label="Percentage For Income(%)" required disabled />
                         </div>
                         <div class="col-md-4">
                             <x-backend.form.text-input type='number' name="asset_percentage"
-                                placeholder="Percentage For Asset" label="Percentage For Asset(%)" disabled />
+                                placeholder="Percentage For Asset" label="Percentage For Asset(%)" required disabled />
                         </div>
                     </div>
 
@@ -122,16 +123,16 @@
                     <div class="row only-individual">
                         <div class="col-md-6">
                             <x-backend.form.text-input type="number" name="tax_free_male" placeholder="Tax Free Male"
-                                label="Tax Free Male(৳)" />
+                                label="Tax Free Male(৳)" required />
                         </div>
                         <div class="col-md-6">
                             <x-backend.form.text-input type="number" name="tax_free_female"
-                                placeholder="Tax Free Female" label="Tax Free Female(৳)" />
+                                placeholder="Tax Free Female" label="Tax Free Female(৳)" required />
                         </div>
                     </div>
                     <div class="row not-individual d-none">
                         <x-backend.form.text-input type="number" name="tax_free" placeholder="Tax Free"
-                            label="Tax Free(৳)" />
+                            label="Tax Free(৳)" required />
                     </div>
 
                 </div>
@@ -183,8 +184,8 @@
                                                 <label class="form-label mb-0 p-0 col-12">Amount</label>
                                                 <div class="d-flex align-items-center justify-content-center border shadow-sm rounded"
                                                     style="overflow: hidden;">
-                                                    <input type="checkbox" checked id="is-discount" name="is_discounts[]"
-                                                        hidden disabled>
+                                                    <input type="text" id="is-discount" name="is_discounts_fixed[]"
+                                                        value="false" hidden disabled>
                                                     <input type="number" id="discount-amount" name="discount_amounts[]"
                                                         class="amount border-0 rounded-0 w-100 ps-2"
                                                         style="outline:transparent;" placeholder="0" aria-label="Discont"
@@ -319,7 +320,7 @@
                     })
                 },
                 discount: {
-                    isFixed: true,
+                    isFixed: false,
                     toggle: function(element) {
                         console.log(element);
                         this.isFixed = !this.isFixed;
