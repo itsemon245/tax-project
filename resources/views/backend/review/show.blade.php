bb2e5e051ee4bd1645616409f49384edf584b9a5
Zabir Emu <zabirraihan570@gmail.com>
2023-10-08 19:46:40 +0600

    completed review view

--- resources/views/backend/review/show.blade.php
+++ resources/views/backend/review/show.blade.php
@@ -1,6 +1,6 @@
 @extends('backend.layouts.app')
 @section('content')
-    @php
+    {{-- @php
         $reviewable_types = [
             'Book' => 'Book',
             'Product' => 'Product',
@@ -9,63 +9,77 @@
             'Service' => 'Service',
             'CaseStudy' => 'Case Study',
         ];
-    @endphp
-    <x-backend.ui.breadcrumbs :list="['Frontend', 'Review', 'Create']" />
-    <x-backend.ui.section-card name="Create Review">
-        <form action="{{ route('review.store', 'Product') }}" method="post">
-            <section class="container">
+    @endphp --}}
+    <x-backend.ui.breadcrumbs :list="['Frontend', 'Review', 'View']" />
+    <x-backend.ui.section-card name="View Review">
+        <div class="review">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="review">
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <x-backend.form.select-input id="reviewable_type" required label="Reviewable Type"
-                            name="reviewable_type" placeholder="Choose type...">
-                            @forelse ($reviewable_types as $key => $reviewable_type)
-                                <option value="{{ $key }}">{{ $reviewable_type }}</option>
-                            @empty
-                                <option disabled>No Records Found!</option>
-                            @endforelse
-                        </x-backend.form.select-input>
-                    </div>
-
-                    <div class="col-md-6">
-                        <x-backend.form.select-input id="reviewable_item" required label="Reviewable Item" name="item_id"
-                            placeholder="Choose item...">
-                            <option disabled>No Records Found!</option>
-                        </x-backend.form.select-input>
-                    </div>
-                    <x-backend.form.text-input type="text" name="name" label="Author Name" required />
-                    {{-- <input type="text"  name="name"> --}}
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <label class="mb-0 form-text fs-6">Give a rating</label>
-                        <div class="rating mb-1">
-                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
-                                data-index="1"></i>
-                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
-                                data-index="2"></i>
-                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
-                                data-index="3"></i>
-                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
-                                data-index="4"></i>
-                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
-                                data-index="5"></i>
-                            <div id="rating-error"></div>
-                        </div>
-                        <div class="mb-3">
-                            <div class="form-inputs">
-                                <input type="hidden" value="2" name="item_id">
-                                <input type="hidden" value="book" name="slug">
-                                <input type="hidden" value="" name="rating">
-                                <textarea class="form-control" rows="3" id="comment" name="comment" placeholder="Input your comment..."></textarea>
-                                <div id="comment-error"></div>
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <span>Avatar:</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <span><img src="{{ $review->avatar }}" alt="" width="200"></span>
+                                </div>
+                                <div class="col-lg-4">
+                                    <span>Author Name:</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <span>{{ $review->user->name }}</span>
+                                </div>
+                                
+                                <div class="col-lg-4">
+                                    <span>Name:</span>
+                                </div>
+                                
+                                <div class="col-lg-8">
+                                    <span>{{ $review->name }}</span>
+                                </div>
+                                
+                                <div class="col-lg-4">
+                                    <span>Comment:</span>
+                                </div>
+                                
+                                <div class="col-lg-8">
+                                    <span> {{ $review->comment }}</span>
+                                </div>
+                                
+                                <div class="col-lg-4">
+                                    <span>Rating:</span>
+                                </div>
+                                
+                                <div class="col-lg-8">
+                                    <span>{{ $review->rating }}</span>
+                                </div>
+                                
+                                <div class="col-lg-4">
+                                    <span>Date:</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    <span>{{ $review->created_at->format('d F Y H:i A') }}</span>
+                                </div>
+                                
+                                <div class="col-lg-4">
+                                    <span>Status</span>
+                                </div>
+                                <div class="col-lg-8">
+                                    @if ($review->status === 1)
+                                        <span class="badge bg-success rounded-pill">Active</span>
+                                    @else
+                                        <span class="badge bg-danger rounded-pill">in-active</span>
+                                    @endif
+                                </div>
                             </div>
+
                         </div>
-                        <button id="submit-button" type="button" class="btn btn-primary btn-sm">Submit</button>
                     </div>
                 </div>
-            </section>
-        </form>
+            </div>
+        </div>
     </x-backend.ui.section-card>
 @endsection
 @push('customJs')
