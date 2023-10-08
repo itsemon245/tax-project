30d88cfe040ba6e9c81b3af0940c55af83828eaa
Zabir Emu <zabirraihan570@gmail.com>
2023-10-08 19:31:43 +0600

    fix bugs management

--- resources/views/backend/review/show.blade.php
+++ resources/views/backend/review/show.blade.php
@@ -1,25 +1,35 @@
 @extends('backend.layouts.app')
 @section('content')
+    @php
+        $reviewable_types = [
+            'Book' => 'Book',
+            'Product' => 'Product',
+            'ExpertProfile' => 'Expert Profile',
+            'Course' => 'Course',
+            'Service' => 'Service',
+            'CaseStudy' => 'Case Study',
+        ];
+    @endphp
     <x-backend.ui.breadcrumbs :list="['Frontend', 'Review', 'Create']" />
     <x-backend.ui.section-card name="Create Review">
         <form action="{{ route('review.store', 'Product') }}" method="post">
             <section class="container">
-                {{ dd($review) }}
+
                 <div class="row">
                     <div class="col-md-6">
-                        <x-backend.form.select-input id="reviewable_type" required label="Reviewable Type" name="reviewable_type"
-                        placeholder="Choose type...">
-                        @forelse ($reviewable_types as $key => $reviewable_type)
-                        <option value="{{ $key }}">{{ $reviewable_type }}</option>
-                        @empty
-                        <option disabled>No Records Found!</option>
-                        @endforelse
+                        <x-backend.form.select-input id="reviewable_type" required label="Reviewable Type"
+                            name="reviewable_type" placeholder="Choose type...">
+                            @forelse ($reviewable_types as $key => $reviewable_type)
+                                <option value="{{ $key }}">{{ $reviewable_type }}</option>
+                            @empty
+                                <option disabled>No Records Found!</option>
+                            @endforelse
                         </x-backend.form.select-input>
                     </div>
-    
+
                     <div class="col-md-6">
                         <x-backend.form.select-input id="reviewable_item" required label="Reviewable Item" name="item_id"
-                        placeholder="Choose item...">
+                            placeholder="Choose item...">
                             <option disabled>No Records Found!</option>
                         </x-backend.form.select-input>
                     </div>
@@ -30,16 +40,16 @@
                     <div class="col-md-12">
                         <label class="mb-0 form-text fs-6">Give a rating</label>
                         <div class="rating mb-1">
-                            <i class="fas fa-star input-star fs-5"
-                                style="color: var(--bs-gray-400);cursor: pointer;" data-index="1"></i>
-                            <i class="fas fa-star input-star fs-5"
-                                style="color: var(--bs-gray-400);cursor: pointer;" data-index="2"></i>
-                            <i class="fas fa-star input-star fs-5"
-                                style="color: var(--bs-gray-400);cursor: pointer;" data-index="3"></i>
-                            <i class="fas fa-star input-star fs-5"
-                                style="color: var(--bs-gray-400);cursor: pointer;" data-index="4"></i>
-                            <i class="fas fa-star input-star fs-5"
-                                style="color: var(--bs-gray-400);cursor: pointer;" data-index="5"></i>
+                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
+                                data-index="1"></i>
+                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
+                                data-index="2"></i>
+                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
+                                data-index="3"></i>
+                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
+                                data-index="4"></i>
+                            <i class="fas fa-star input-star fs-5" style="color: var(--bs-gray-400);cursor: pointer;"
+                                data-index="5"></i>
                             <div id="rating-error"></div>
                         </div>
                         <div class="mb-3">
@@ -59,58 +69,59 @@
     </x-backend.ui.section-card>
 @endsection
 @push('customJs')
-<script>
-    $(document).ready(function() {
-        let action = "{{ route('review.store', 'SLUG') }}";
-        $('#reviewable_type').on('change', function() {
-            var selectedValue = $(this).val();
-            action = action.replace('SLUG', selectedValue);
-            let url = "{{ route('ajax.get.items', 'SLUG') }}";
-            url = url.replace('SLUG', selectedValue);
+    <script>
+        $(document).ready(function() {
+            let action = "{{ route('review.store', 'SLUG') }}";
+            $('#reviewable_type').on('change', function() {
+                var selectedValue = $(this).val();
+                action = action.replace('SLUG', selectedValue);
+                let url = "{{ route('ajax.get.items', 'SLUG') }}";
+                url = url.replace('SLUG', selectedValue);
                 $.ajax({
-                type: 'GET', 
-                url: url,
-                success: function(response) {
-                    $('#reviewable_item').children().remove()
-                   response.forEach(element => {
-                       let option = `<option value="${element.id}"> ${element.title ?? element.name} </option>`
-                       $('#reviewable_item').append(option)
-                   });
+                    type: 'GET',
+                    url: url,
+                    success: function(response) {
+                        $('#reviewable_item').children().remove()
+                        response.forEach(element => {
+                            let option =
+                                `<option value="${element.id}"> ${element.title ?? element.name} </option>`
+                            $('#reviewable_item').append(option)
+                        });
 
-                },
+                    },
+                });
             });
-        });
-        $('#submit-button').click(function(e) {
-            let comment = $('[name="comment"]').val();
-            let name = $('[name="name"]').val();
-            let rating = $('input[name="rating"]').val();
-            let itemId = $('input[name="item_id"]').val();
+            $('#submit-button').click(function(e) {
+                let comment = $('[name="comment"]').val();
+                let name = $('[name="name"]').val();
+                let rating = $('input[name="rating"]').val();
+                let itemId = $('input[name="item_id"]').val();
 
-            $.ajax({
-                method: "post",
-                url: action,
-                data: {
-                    'item_id': itemId,
-                    "comment": comment,
-                    'rating': rating,
-                    'name' : name,
-                },
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                success: function(response) {
-                    console.log(response);
-                    if (response.success) {
-                        let data = response.data
-                        let icons = '';
-                        for (let i = 1; i < 6; i++) {
-                            let color = i > data.rating ? 'var(--bs-gray-200)' :
-                                'var(--bs-yellow)'
-                            icons +=
-                                `\n<span class="fas fa-star" style="color:${color};"></span>`
-                        }
+                $.ajax({
+                    method: "post",
+                    url: action,
+                    data: {
+                        'item_id': itemId,
+                        "comment": comment,
+                        'rating': rating,
+                        'name': name,
+                    },
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function(response) {
+                        console.log(response);
+                        if (response.success) {
+                            let data = response.data
+                            let icons = '';
+                            for (let i = 1; i < 6; i++) {
+                                let color = i > data.rating ? 'var(--bs-gray-200)' :
+                                    'var(--bs-yellow)'
+                                icons +=
+                                    `\n<span class="fas fa-star" style="color:${color};"></span>`
+                            }
 
-                        let review = ` 
+                            let review = ` 
                             <div class="d-flex gap-3 align-items-start border p-3 rounded-3 mb-3">
                                 <img loading="lazy" src="${data.avatar}" alt="img"
                                     width="64px" height="64px" class=" rounded-circle shadow-4-strong d-block">
@@ -128,47 +139,44 @@
                             </div>
                         `
 
-                        $('.no-review').remove()
-                        $('.review-list').prepend(review)
+                            $('.no-review').remove()
+                            $('.review-list').prepend(review)
 
-                        Toast.fire({
-                            icon: "success",
-                            title: response.message
-                        })
-                        console.log(response);
-                    } else {
-                        Toast.fire({
-                            icon: "error",
-                            title: response.message
-                        })
+                            Toast.fire({
+                                icon: "success",
+                                title: response.message
+                            })
+                            console.log(response);
+                        } else {
+                            Toast.fire({
+                                icon: "error",
+                                title: response.message
+                            })
+                        }
+                    },
+                    error: function(err) {
+                        let errors = err.responseJSON.errors
+                        if (errors.rating) {
+                            $('#rating-error').html(
+                                `<span class="text-danger">${errors.rating}</span>`)
+                        }
+                        if (errors.comment) {
+                            $('#comment-error').html(
+                                `<span class="text-danger">${errors.comment}</span>`)
+                        }
                     }
-                },
-                error: function(err) {
-                    let errors = err.responseJSON.errors
-                    if (errors.rating) {
-                        $('#rating-error').html(
-                            `<span class="text-danger">${errors.rating}</span>`)
-                    }
-                    if (errors.comment) {
-                        $('#comment-error').html(
-                            `<span class="text-danger">${errors.comment}</span>`)
-                    }
-                }
-            });
-        })
-        $('.input-star').click(e => {
-            let yellow = 'var(--ct-yellow)'
-            let gray = 'var(--ct-gray-400)'
-            let icon = $(e.target)
-            let value = e.target.dataset.index
-            icon.prevAll().css('color', yellow)
-            icon.css('color', yellow)
-            icon.nextAll().css('color', gray)
-            $('input[name="rating"]').val(value)
-        })
-    });
-</script>
+                });
+            })
+            $('.input-star').click(e => {
+                let yellow = 'var(--ct-yellow)'
+                let gray = 'var(--ct-gray-400)'
+                let icon = $(e.target)
+                let value = e.target.dataset.index
+                icon.prevAll().css('color', yellow)
+                icon.css('color', yellow)
+                icon.nextAll().css('color', gray)
+                $('input[name="rating"]').val(value)
+            })
+        });
+    </script>
 @endpush
-
-
-
