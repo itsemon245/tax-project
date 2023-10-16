22ef5ae9406e10ff6d587d5218ead299e7ca1617
Mojahidul Islam <bdemon245@gmail.com>
2023-10-16 11:52:10 +0600

    bug-fix: table row disappearing bug in windows

--- resources/views/backend/project/projectClients.blade.php
+++ resources/views/backend/project/projectClients.blade.php
@@ -20,149 +20,8 @@
             <x-backend.ui.button type="custom" href="{{ route('project.index') }}"
                 class="btn-success btn-sm">Back</x-backend.ui.button>
         </div>
-        {{-- <div class="row border-bottom mb-1">
-            <div class="col-md-12">
-                <div id="progressbarwizard">
-                    <div class="d-flex justify-content-center">
-                        <ul class="nav nav-pills bg-light nav-justified form-wizard-header w-100" role="tablist">
-                            <li class="nav-item" role="presentation">
-                                <a href="#daily" data-bs-toggle="tab" data-toggle="tab" class="nav-link rounded-0 active"
-                                    aria-selected="true" role="tab" tabindex="-1">
-                                    <i class="mdi mdi-clock-time-two-outline"></i>
-                                    <span class="d-none d-sm-inline">Daily</span>
-                                </a>
-                            </li>
-                            <li class="nav-item" role="presentation">
-                                <a href="#weekly" data-bs-toggle="tab" data-toggle="tab" class="nav-link rounded-0"
-                                    aria-selected="false" role="tab" tabindex="-1">
-                                    <i class="mdi mdi-table-clock"></i>
-                                    <span class="d-none d-sm-inline">Weekly</span>
-                                </a>
-                            </li>
-                            <li class="nav-item" role="presentation">
-                                <a href="#monthly" data-bs-toggle="tab" data-toggle="tab" class="nav-link rounded-0"
-                                    aria-selected="false" role="tab" tabindex="-1">
-                                    <i class="mdi mdi-table-clock"></i>
-                                    <span class="d-none d-sm-inline">Monthly</span>
-                                </a>
-                            </li>
-                            <li class="nav-item" role="presentation">
-                                <a href="#total" data-bs-toggle="tab" data-toggle="tab" class="nav-link rounded-0"
-                                    aria-selected="false" role="tab" tabindex="-1">
-                                    <i class="mdi mdi-table-clock"></i>
-                                    <span class="d-none d-sm-inline">Total</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="tab-content ">
-                        <div class="tab-pane my-3 active" id="daily" role="tabpanel">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    @forelse ($projects as $project)
-                                        @php
-                                            $progress = $project->daily_target === 0 ? 100 : ($project->daily_progress * 100) / $project->daily_target;
-                                            $progress = round($progress);
-                                            $color = match (true) {
-                                                $progress <= 30 => 'bg-danger',
-                                                $progress <= 60 => 'bg-warning',
-                                                default => 'bg-success',
-                                            };
-                                        @endphp
-                                        <span class="text-dark">{{ $project->name }}:</span>
-                                        <div id="bar" class="progress mb-2 w-100" style="height: 15px;">
-                                            <div
-                                                class="bar progress-bar progress-bar-striped progress-bar-animated {{ $color }}%;">
-                                                <span class="text-light">{{ $progress }}%</span>
-                                            </div>
-                                        </div>
-                                    @empty
-                                        <h5 class="d-flex justify-content-center text-muted">No record found</h5>
-                                    @endforelse
-                                </div>
-                            </div> <!-- end row -->
-                        </div>
-                        <div class="tab-pane my-3" id="weekly" role="tabpanel">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    @forelse ($projects as $project)
-                                        @php
-                                            $progress = $project->weekly_target === 0 ? 100 : ($project->weekly_progress * 100) / $project->weekly_target;
-                                            $progress = round($progress);
-                                            $color = match (true) {
-                                                $progress <= 30 => 'bg-danger',
-                                                $progress <= 60 => 'bg-warning',
-                                                default => 'bg-success',
-                                            };
-                                        @endphp
-                                        <span class="text-dark">{{ $project->name }}:</span>
-                                        <div id="bar" class="progress mb-2 w-100" style="height: 15px;">
-                                            <div class="bar progress-bar progress-bar-striped progress-bar-animated {{ $color }}"
-                                                style="width: {{ $progress }}%;"><span
-                                                    class="text-light">{{ $progress }}%</span></div>
-                                        </div>
-                                    @empty
-                                        <h5 class="d-flex justify-content-center text-muted">No record found</h5>
-                                    @endforelse
-                                </div>
-                            </div> <!-- end row -->
-                        </div>
-                        <div class="tab-pane my-3" id="monthly" role="tabpanel">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    @forelse ($projects as $project)
-                                        @php
-                                            $progress = $project->monthly_target === 0 ? 100 : ($project->monthly_progress * 100) / $project->monthly_target;
-                                            $progress = round($progress);
-                                            $color = match (true) {
-                                                $progress <= 30 => 'bg-danger',
-                                                $progress <= 60 => 'bg-warning',
-                                                default => 'bg-success',
-                                            };
-                                        @endphp
-                                        <span class="text-dark">{{ $project->name }}:</span>
-                                        <div id="bar" class="progress mb-2 w-100" style="height: 15px;">
-                                            <div class="bar progress-bar progress-bar-striped progress-bar-animated {{ $color }}"
-                                                style="width: {{ $progress }}%;"><span
-                                                    class="text-light">{{ $progress }}%</span></div>
-                                        </div>
-                                    @empty
-                                        <h5 class="d-flex justify-content-center text-muted">No record found</h5>
-                                    @endforelse
-                                </div>
-                            </div> <!-- end row -->
-                        </div>
-                        <div class="tab-pane my-3" id="total" role="tabpanel">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    @forelse ($projects as $project)
-                                        @php
-                                            $progress = $project->total_clients === 0 ? 100 : ($project->total_progress * 100) / $project->total_clients;
-                                            $progress = round($progress);
-                                            $color = match (true) {
-                                                $progress <= 30 => 'bg-danger',
-                                                $progress <= 60 => 'bg-warning',
-                                                default => 'bg-success',
-                                            };
-                                        @endphp
-                                        <span class="text-dark">{{ $project->name }}:</span>
-                                        <div id="bar" class="progress mb-2 w-100" style="height: 15px;">
-                                            <div class="bar progress-bar progress-bar-striped progress-bar-animated {{ $color }}"
-                                                style="width:{{ $progress }}%;"><span
-                                                    class="text-light">{{ $progress }}%</span></div>
-                                        </div>
-                                    @empty
-                                        <h5 class="d-flex justify-content-center text-muted">No record found</h5>
-                                    @endforelse
-                                </div>
-                            </div> <!-- end row -->
-                        </div>
-                    </div> <!-- tab-content -->
-                </div>
-            </div>
-        </div> --}}
-        
-        <x-backend.table.basic :items="$project?->clients">
+
+        <x-backend.table.basic :items="$clients">
             <thead>
                 <tr>
                     <th>#</th>
@@ -178,7 +37,7 @@
                 </tr>
             </thead>
             <tbody>
-                @if ($project?->clients->count() > 0)
+                @if ($clients->count() > 0)
                     @forelse ($project->clients as $key=>$client)
                         <tr>
                             <td>{{ ++$key }}</td>
