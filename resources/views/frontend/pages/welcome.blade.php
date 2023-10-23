d298ae1a2f7c079c28de67747c4a142fe153745e
Mojahidul Islam <bdemon245@gmail.com>
2023-10-23 14:50:12 +0600

    minor ui fixes

--- resources/views/frontend/pages/welcome.blade.php
+++ resources/views/frontend/pages/welcome.blade.php
@@ -98,10 +98,10 @@
         <h4 class="text-center mb-5 fs-3">Our Achievments</h4>
         <div class="row justify-content-center px-2">
             @foreach ($achievements as $item)
-                <div class="col-sm-6 col-md-4 col-xl-3 col-xxl-2 mb-2">
-                    <div class="card rounded-2">
+                <div class="col-6 col-md-4 col-xl-3 col-xxl-2 mb-2 align-items-center justify-content-center">
+                    <div class="card rounded- h-100">
                         <div class="card-body p-2">
-                            <div class="d-flex gap-3 align-items-center">
+                            <div class="d-flex gap-3 align-items-center justify-content-center">
                                 <img loading="lazy" class="rounded rounded-2" style="width:80px;height:80px;"
                                     src="{{ useImage($item->image) }}" alt="">
                                 <div class="d-flex flex-column justify-content-starr align-items-start">
