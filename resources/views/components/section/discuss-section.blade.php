6de225e8b3cb05d64335925570d6dbbd3b5ba94e
Mojahidul Islam <bdemon245@gmail.com>
2023-11-11 15:53:53 +0600

    extracted discussion section

---
+++ resources/views/components/section/discuss-section.blade.php
@@ -0,0 +1,34 @@
+<div>
+    <div class="mx-5 my-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mb-3 mb-md-0">
+                    <img loading="lazy" style="object-fit: cover;" class="rounded shadow w-100 h-100"
+                        src="{{ asset('frontend/assets/images/small/img-6.jpg') }}" />
+                </div>
+                <div class="col-md-6">
+                    <div class="bg-light p-5 rounded shadow w-100 h-100">
+                        <h3 class="my-3">Let's Discuss your project</h3>
+                        <p class="mb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, pariatur. Ex,
+                            nobis. Ratione
+                            adipisci,
+                            nemo
+                            vitae dolore soluta cupiditate, optio officia, accusamus dignissimos unde quaerat? Lorem ipsum
+                            dolor
+                            sit
+                            amet consectetur adipisicing elit. Iusto doloribus tempora et ipsam quo ullam
+                        </p>
+                        <div class="d-flex justify-content-center">
+                            <a href="{{ route('contact') }}" class="btn btn-primary"
+                                style="font-weight: 500; padding:0.8rem;">
+                                <i class="fe-users"></i>
+                                TALK TO OUR EXPERTS
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/frontend/pages/clientStudio/clientStudio.blade.php
+++ resources/views/frontend/pages/clientStudio/clientStudio.blade.php
@@ -1,6 +1,6 @@
 @php
     $partners = getRecords('partner_sections');
-    
+
 @endphp
 @extends('frontend.layouts.app')
 @section('main')
@@ -43,39 +43,7 @@
         </div>
     </div>
 
-    {{-- Lets discuss --}}
-    <div class="mx-5 my-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 mb-3 mb-md-0">
-                    <img loading="lazy" style="object-fit: cover;" class="rounded shadow w-100 h-100"
-                        src="{{ asset('frontend/assets/images/small/img-6.jpg') }}" />
-                </div>
-                <div class="col-md-6">
-                    <div class="bg-light p-5 rounded shadow w-100 h-100">
-                        <h3 class="my-3">Let's Discuss your project</h3>
-                        <p class="mb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, pariatur. Ex,
-                            nobis. Ratione
-                            adipisci,
-                            nemo
-                            vitae dolore soluta cupiditate, optio officia, accusamus dignissimos unde quaerat? Lorem ipsum
-                            dolor
-                            sit
-                            amet consectetur adipisicing elit. Iusto doloribus tempora et ipsam quo ullam
-                        </p>
-                        <div class="d-flex justify-content-center">
-                            <a href="{{ route('contact') }}" class="btn btn-primary"
-                                style="font-weight: 500; padding:0.8rem;">
-                                <i class="fe-users"></i>
-                                TALK TO OUR EXPERTS
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-section.discuss-section />
     <section class="mt-5 py-5" style="background: #474646;">
         <h3 class="text-light text-center">Our Valuable Partners</h3>
         <div class="scroll-wrapper">
@@ -88,8 +56,9 @@
                     <div class="media-elements">
                         <div class="d-flex align-items-start gap-3 p-3" style="width: 100%;">
                             <div>
-                                <img loading="lazy" class="border image rounded-circle" src="{{ useImage($partner->image) }}"
-                                    width="80px" height="80px" style="object-fit: cover" alt="">
+                                <img loading="lazy" class="border image rounded-circle"
+                                    src="{{ useImage($partner->image) }}" width="80px" height="80px"
+                                    style="object-fit: cover" alt="">
 
                             </div>
                             <div>
