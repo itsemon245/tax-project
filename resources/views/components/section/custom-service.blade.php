43e5d2e816e8637c2a1b835f466338642572364e
Mojahidul Islam <bdemon245@gmail.com>
2023-11-28 16:34:56 +0600

    fix:custom service column

--- resources/views/components/section/custom-service.blade.php
+++ resources/views/components/section/custom-service.blade.php
@@ -2,7 +2,7 @@
     <h4 class="text-center my-5" style="font-size:28px; font-weight:600;">Services</h4>
     <div class="row mx-lg-5 mx-2 service-category justify-content-center">
         @foreach ($customServices as $sub)
-            <div class="col-md-4 col-lg-3 col-sm-6 mb-3">
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
                 <div class="d-flex flex-column align-items-center border rounded shadow-sm p-2 h-100">
                     <a href="{{ $sub->link }}">
                         <img loading="lazy" style="width:clamp(80px, 120px, 150px);aspect-ratio:1/1;"
