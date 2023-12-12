a393d3298a52a5fb2f2b64a132009a66aa32e4ec
mdparvej45 <pj.parvaz45@gmail.com>
2023-12-12 23:07:25 +0600

    client side: contact developer page

--- resources/views/frontend/pages/contact-developers.blade.php
+++ resources/views/frontend/pages/contact-developers.blade.php
@@ -6,13 +6,66 @@
         <div class="col-md-12 mt-5">
             <div class="row">
                 <div class="col-md-6">
-                    <div class="card" style="background-image: linear-gradient(rgba(0, 128, 0, 0.224), rgba(0, 0, 255, 0.191), rgba(255, 166, 0, 0.168))">
+                    <div class="card" >
                         <div class="card-body row">
                             <div class="col-md-4">
-                                <img loading="lazy" class="border border-5 border-primary" id="liveImage" style=" height:100%; width:100%; border-radius:50%;" src="" alt="Mojahidul Islam Image">
+                                <img loading="lazy" class="border border-5 border-primary" id="liveImage" style=" height:100%; width:100%; border-radius:50%;" src="{{ asset('images/developer_3.jpg') }}" alt="Mojahidul Islam Image">
                             </div>
-                            <div class="col-md-8"></div>
-                            
+                                            <div class="col-md-8">
+                                <h3 class="mt-3">Mojahidul Islam Emon </h3>
+                                <h5>Mobile: +8801643428395</h5>
+                                <h5>Email: +8801612418511</h5>
+                                <div class="flex">
+                                    <span class="fs-5">
+                                        <a href="http://"><i class="fab fa-facebook"></i></a> 
+                                        <a href="http://"><i class="fab fa-linkedin-in"></i></a> 
+                                    </span>
+                                </div>
+                            </div> 
+                        </div>
+                        <span class="mdi mdi-graph-outline p-0 m-0"
+                            style="color: var(--bs-gray-500);position: absolute;top:0;right:4px;"></span>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card" >
+                        <div class="card-body row">
+                            <div class="col-md-4">
+                                <img loading="lazy" class="border border-5 border-primary" id="liveImage" style=" height:100%; width:100%; border-radius:50%;" src="{{ asset('images/developer_2.jpg') }}" alt="Mojahidul Islam Image">
+                            </div>
+                            <div class="col-md-8">
+                                <h3 class="mt-3">Md Parvez Ahmmed </h3>
+                                <h5>Mobile: +8801885518864</h5>
+                                <h5>Email: pj.parvaz45@gmail.com</h5>
+                                <div class="flex">
+                                    <span class="fs-5">
+                                        <a href="http://"><i class="fab fa-facebook"></i></a> 
+                                        <a href="http://"><i class="fab fa-linkedin-in"></i></a> 
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <span class="mdi mdi-graph-outline p-0 m-0"
+                            style="color: var(--bs-gray-500);position: absolute;top:0;right:4px;"></span>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card" >
+                        <div class="card-body row">
+                            <div class="col-md-4">
+                                <img loading="lazy" class="border border-5 border-primary" id="liveImage" style=" height:100%; width:100%; border-radius:50%;" src="{{ asset('images/developer_1.jpg') }}" alt="Mojahidul Islam Image">
+                            </div>
+                            <div class="col-md-8">
+                                <h3 class="mt-3">Zabir Emu </h3>
+                                <h5>Mobile: +8801612418511</h5>
+                                <h5>Email: +8801612418511</h5>
+                                <div class="flex">
+                                    <span class="fs-5">
+                                        <a href="http://"><i class="fab fa-facebook"></i></a> 
+                                        <a href="http://"><i class="fab fa-linkedin-in"></i></a> 
+                                    </span>
+                                </div>
+                            </div>                            
                         </div>
                         <span class="mdi mdi-graph-outline p-0 m-0"
                             style="color: var(--bs-gray-500);position: absolute;top:0;right:4px;"></span>
