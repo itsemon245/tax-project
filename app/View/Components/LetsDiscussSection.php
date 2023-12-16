e14cb3dbfd3165a780d84b3483a42c979daa0b1b
Mojahidul Islam <bdemon245@gmail.com>
2023-12-16 09:29:20 +0600

    fix: class name

--- app/View/Components/LetsDiscussSection.php
+++ app/View/Components/LetsDiscussSection.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class lets-discuss-section extends Component
+class LetsDiscussSection extends Component
 {
     /**
      * Create a new component instance.
