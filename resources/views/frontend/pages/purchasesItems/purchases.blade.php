ec6a7d2f8cc9e162c86634e5d64110b8b8f86138
mdparvej45 <pj.parvaz45@gmail.com>
2024-02-06 20:44:05 +0600

    fix my purchase & course.

--- resources/views/frontend/pages/purchasesItems/purchases.blade.php
+++ resources/views/frontend/pages/purchasesItems/purchases.blade.php
@@ -4,20 +4,21 @@
     @push('customCss')
     @endpush
     {{-- Services --}}
-    @section('main')
+@section('main')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-2">
-                <h4 class="my-5" style="font-size:28px; font-weight:600;">Purchase Items
-                </h4>
-            </div>
-            <div class="col-sm-8 col-xl-3 mb-3">
+        <h4 class="my-5" style="font-size:28px; font-weight:600;">Purchase Items
+        </h4>
+
+        <div class="row mb-3" >
             @forelse ($purchaseItem  as $item)
             @php
-            $model = app('App\Models\\' . $item->purchasable_type);
-            $modelTableRowID = $model::find($item->id);
+                $model = app('App\Models\\' . $item->purchasable_type);
+                $modelTableRowID = $model::find($item->id);
             @endphp
+           <div class="col-md-4">
             <div class="w-100 h-100">
+                <h6 class="{{ $item->approved == 1 ? 'text-success' : 'text-danger' }}">
+                    {{ $item->approved == 1 ? 'Approved.' : 'Pendding for Approved.' }}</h6>
                 <div class="d-flex flex-column align-items-center border p-3">
                     <span class="text-muted text-start">
                         {{ $item->purchasable_type }}
@@ -34,7 +35,7 @@
                         {{ $item->name }}
                     </span>
                     <span class=" text-muted">
-                    Contact Number: {{ $item->contact_number }}
+                        Contact Number: {{ $item->contact_number }}
                     </span>
                     <span class="text-muted">
                         Transaction Id: {{ $item->trx_id }}
@@ -42,12 +43,12 @@
                     <span class="text-muted">
                         Payment Date: {{ $item->payment_date }}
                     </span>
-                    <span  class=" text-muted">
+                    <span class=" text-muted">
                         Expire Date: {{ $item->expire_date }}
                     </span>
                 </div>
                 <div class="bg-primary p-2 custom-grid">
-                    <span  class="mb-1 text-black d-block">
+                    <span class="mb-1 text-black d-block">
                         Payble Amount: {{ $item->payable_amount }}
                     </span>
                     <span class="mb-1 text-black  d-block">
@@ -58,24 +59,24 @@
                     </span>
                 </div>
             </div>
-            @empty
+           </div>
+        @empty
             <div class="row">
                 <div class="col-md-2"></div>
-                <div class="col-md-8" style="max-width:800px;">
-                    <img loading="lazy" src="{{ asset('frontend/assets/images/no_data.jpg') }}" style="height:100%;" class="img-fluid p-5" alt="Responsive image">
+                <div class="col-md-8">
+                    <img loading="lazy" class="w-100" src="{{ asset('frontend/assets/images/no_data.jpg') }}"
+                        style="height:100%;" class="img-fluid p-5" alt="Responsive image">
                 </div>
                 <div class="col-md-2"></div>
             </div>
-            @endforelse
-        </div>
-            <div class="col-md-2"></div>
+        @endforelse
         </div>
     </div>
-    @endsection
+@endsection
 
-    {{-- <section class="px-lg-12 px-2 my-5">
+{{-- <section class="px-lg-12 px-2 my-5">
 
     </section> --}}
-    @push('customJs')
-    @endpush
+@push('customJs')
+@endpush
 @endsection
