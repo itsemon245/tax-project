5760c4b9c7ac2b2c3ccc33c53dbce8f0ed64aa1a
Mojahidul Islam <bdemon245@gmail.com>
2024-02-08 01:18:59 +0600

    fix: purchase history

--- resources/views/frontend/pages/purchasesItems/purchases.blade.php
+++ resources/views/frontend/pages/purchasesItems/purchases.blade.php
@@ -6,70 +6,57 @@
     {{-- Services --}}
 @section('main')
     <div class="container">
-        <h4 class="my-5" style="font-size:28px; font-weight:600;">Purchase Items
+        <h4 class="mt-5 mb-3" style="font-size:28px; font-weight:600;">Purchase Items
         </h4>
 
-        <div class="row mb-3" >
+        <div class="d-flex justify-content-start align-items-center gap-4 mb-3">
             @forelse ($purchaseItem  as $item)
-            @php
-                $model = app('App\Models\\' . $item->purchasable_type);
-                $modelTableRowID = $model::find($item->id);
-            @endphp
-           <div class="col-md-4">
-            <div class="w-100 h-100">
-                <h6 class="{{ $item->approved == 1 ? 'text-success' : 'text-danger' }}">
-                    {{ $item->approved == 1 ? 'Approved.' : 'Pendding for Approved.' }}</h6>
-                <div class="d-flex flex-column align-items-center border p-3">
-                    <span class="text-muted text-start">
-                        {{ $item->purchasable_type }}
-                    </span>
-                    <span class="text-dark text-capitalize" href="">
-                        @if ($modelTableRowID->title)
-                            <h6>{{ $modelTableRowID->title }}</h6>
-                        @elseif($modelTableRowID->name)
-                            <h6>{{ $modelTableRowID->name }}</h6>
-                        @endif
-
-                    </span>
-                    <span class=" text-muted">
-                        {{ $item->name }}
-                    </span>
-                    <span class=" text-muted">
-                        Contact Number: {{ $item->contact_number }}
-                    </span>
-                    <span class="text-muted">
-                        Transaction Id: {{ $item->trx_id }}
-                    </span>
-                    <span class="text-muted">
-                        Payment Date: {{ $item->payment_date }}
-                    </span>
-                    <span class=" text-muted">
-                        Expire Date: {{ $item->expire_date }}
-                    </span>
+                <div class="" style="width: max-content;">
+                    <div class="w-100 h-100 rounded-2" style="overflow: hidden;">
+                        <div class="align-items-center border p-3">
+                            <span class="ms-auto fw-bold mb-2 {{ $item->approved == 1 ? 'text-success' : 'text-danger' }}">
+                                {{ $item->approved == 1 ? 'Approved.' : 'Waiting for Approval.' }}</span>
+                            <div class="text-muted text-start">
+                                Purchase Type: <b>{{ $item->purchasable_type }}</b>
+                            </div>
+                            <div class="text-muted text-start">
+                                {{ $item->purchasable_type }} Name:
+                                <b>{{ $item?->purchasable?->title ?? $item?->purchasable?->name }}</b>
+                            </div>
+                            <div class="text-muted text-start">
+                                Payment Date: <b>{{ \Carbon\Carbon::parse($item->payment_date)->format('d F, Y') }}</b>
+                            </div>
+                            <div class="text-muted text-start">
+                                Transaction Id: <b>{{ $item->trx_id }}</b>
+                            </div>
+                            <div class="text-muted text-start">
+                                Expire Date:
+                                <b>{{ $item->expire_date ? \Carbon\Carbon::parse($item->expire_date)->format('d F, Y') : 'After Approval' }}</b>
+                            </div>
+                        </div>
+                        <div class="bg-primary p-3 w-full">
+                            <div class="w-full mb-1 text-black d-flex justify-contents-between">
+                                Payble Amount: <b class="ms-auto"> {{ $item->payable_amount }} BDT</b>
+                            </div>
+                            <div class="w-full mb-1 text-black  d-flex justify-contents-between">
+                                Paid Amount: <b class="ms-auto">{{ $item->paid ?? '0.00' }} BDT</b>
+                            </div>
+                            <div class="w-full mb-1 text-black  d-flex justify-contents-between">
+                                Due Amount: <b class="ms-auto">{{ $item->due }} BDT</b>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="bg-primary p-2 custom-grid">
-                    <span class="mb-1 text-black d-block">
-                        Payble Amount: {{ $item->payable_amount }}
-                    </span>
-                    <span class="mb-1 text-black  d-block">
-                        Paid Amount: {{ $item->paid }}
-                    </span>
-                    <span class="mb-1 text-black  d-block">
-                        Due Amount: {{ $item->due }}
-                    </span>
+            @empty
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8">
+                        <img loading="lazy" class="w-100" src="{{ asset('frontend/assets/images/no_data.jpg') }}"
+                            style="height:100%;" class="img-fluid p-5" alt="Responsive image">
+                    </div>
+                    <div class="col-md-2"></div>
                 </div>
-            </div>
-           </div>
-        @empty
-            <div class="row">
-                <div class="col-md-2"></div>
-                <div class="col-md-8">
-                    <img loading="lazy" class="w-100" src="{{ asset('frontend/assets/images/no_data.jpg') }}"
-                        style="height:100%;" class="img-fluid p-5" alt="Responsive image">
-                </div>
-                <div class="col-md-2"></div>
-            </div>
-        @endforelse
+            @endforelse
         </div>
     </div>
 @endsection
