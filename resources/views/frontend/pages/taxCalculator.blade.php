a1144e9090b67bdff78c099820e190444a983ecd
Mojahidul Islam <itsemon245@gmail.com>
2024-04-21 00:20:32 +0600

    tax-calculator page re-arranged

--- resources/views/frontend/pages/taxCalculator.blade.php
+++ resources/views/frontend/pages/taxCalculator.blade.php
@@ -53,108 +53,103 @@
                         <div class="tab-pane fade {{ $index === 1 ? 'show active' : '' }}" id="{{ $for }}"
                             role="tabpanel" aria-labelledby="{{ $for . '-tab' }}">
                             <div class="container rounded bg-white py-3 px-4">
-                                <form action="{{ route('tax.calculate') }}" id="form-{{$index}}" method="POST" class="row">
+                                <form action="{{ route('tax.calculate') }}" id="form-{{ $index }}" method="POST"
+                                    class="row">
                                     @csrf
-                                    <div class="col-md-12">
-                                        <div class="row">
-                                            <input hidden value="{{ $for }}" name="tax_for" class="d-none">
-                                            <div class="col-md-6">
-                                                <x-backend.form.text-input label="Name" :value="old('name') ? auth()->user()->name : ''" required
-                                                    type="text" name="name">
-                                                </x-backend.form.text-input>
+                                    <div class="row">
+                                        <input hidden value="{{ $for }}" name="tax_for" class="d-none">
+                                        <div class="col-md-6">
+                                            <h5>General Information</h5>
+                                            <x-backend.form.text-input label="Name" :value="old('name') ? auth()->user()->name : ''" required
+                                                type="text" name="name">
+                                            </x-backend.form.text-input>
+                                            <x-backend.form.text-input label="Email Address" :value="old('email') ? auth()->user()->email : ''"
+                                                type="email" name="email" required>
+                                            </x-backend.form.text-input>
+                                            <x-backend.form.text-input label="Phone Number" :value="old('phone') ? auth()->user()->phone : ''" type="number"
+                                                name="phone" required>
+                                            </x-backend.form.text-input>
+
+                                            <x-backend.form.text-input label="Source of Income" :value="old('income_source')"
+                                                type="text" name="income_source">
+                                            </x-backend.form.text-input>
+
+                                            <div>
+                                                <label for="gender" class="d-block form-label">Gender</label>
+                                                <label for="male" class=" mx-2">
+                                                    <input id="male" type="radio" name="gender" value="male">
+                                                    Male
+                                                </label>
+                                                <label for="female" class=" mx-2">
+                                                    <input id="female" type="radio" name="gender" value="female">
+                                                    Female
+                                                </label>
                                             </div>
-                                            <div class="col-md-6">
-                                                <x-backend.form.text-input label="Email Address" :value="old('email') ? auth()->user()->email : ''"
-                                                    type="email" name="email" required>
-                                                </x-backend.form.text-input>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <x-backend.form.text-input label="Phone Number" :value="old('phone') ? auth()->user()->phone : ''"
-                                                    type="number" name="phone" required>
-                                                </x-backend.form.text-input>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <x-backend.form.text-input label="Source of Income" :value="old('income_source')"
-                                                    type="text" name="income_source">
-                                                </x-backend.form.text-input>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <x-backend.form.text-input label="Yearly Turnover" :value="old('yearly_turnover')"
-                                                    name="yearly_turnover">
-                                                </x-backend.form.text-input>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <x-backend.form.text-input label="Total Assets" type="number"
-                                                    :value="old('total_asset')" name="total_asset">
-                                                </x-backend.form.text-input>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <x-backend.form.text-input label="Yearly Income" :value="old('yearly_income')"
-                                                    name="yearly_income">
-                                                </x-backend.form.text-input>
-                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <h5>Tax & Computation Information</h5>
+                                            <x-backend.form.text-input label="Yearly Turnover" :value="old('yearly_turnover')"
+                                                name="yearly_turnover">
+                                            </x-backend.form.text-input>
+                                            <x-backend.form.text-input label="Total Assets" type="number" :value="old('total_asset')"
+                                                name="total_asset">
+                                            </x-backend.form.text-input>
+                                            <x-backend.form.text-input label="Yearly Income" :value="old('yearly_income')"
+                                                name="yearly_income">
+                                            </x-backend.form.text-input>
+                                            <x-backend.form.text-input label="Tax Deduction" type="number"
+                                                :value="old('deduction')" name="deduction">
+                                            </x-backend.form.text-input>
                                             @if ($for === 'individual')
-                                                <div class="col-md-6">
-                                                    <x-backend.form.text-input label="Investment of Rebate"
-                                                        :value="old('rebate')" name="rebate">
-                                                    </x-backend.form.text-input>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <label for="gender" class="d-block form-label">Gender</label>
-                                                    <label for="male" class=" mx-2">
-                                                        <input id="male" type="radio" name="gender" value="male">
-                                                        Male
-                                                    </label>
-                                                    <label for="female" class=" mx-2">
-                                                        <input id="female" type="radio" name="gender" value="female">
-                                                        Female
-                                                    </label>
-                                                </div>
-                                            @endif
-                                            <div class="col-md-6">
-                                                <x-backend.form.text-input label="Tax Deduction" type="number"
-                                                    :value="old('deduction')" name="deduction">
+                                                <x-backend.form.text-input label="Investment of Rebate" :value="old('rebate')"
+                                                    name="rebate">
                                                 </x-backend.form.text-input>
-                                            </div>
+                                            @endif
 
-                                            <div class="col-12 my-4">
-                                                <h4 class="mb-2">Tax Services</h4>
-                                                <div class="clearfix d-flex flex-wrap gap-3 align-items-center">
-                                                    @php
-                                                        $services = \App\Models\TaxSetting::where([
-                                                            'for' => $for,
-                                                            'type' => 'others',
-                                                        ])->get(['service', 'id']);
-                                                    @endphp
-                                                    @foreach ($services as $key => $service)
-                                                        @if ($service)
-                                                            <label
-                                                                class="form-check-label d-flex gap-2 py-2 px-3 bg-light bg-gradient rounded-3"
-                                                                for="{{ 'service-' . $key }}">
-                                                                <input class="form-check-input" type="checkbox" hidden
-                                                                    id="{{ 'service-' . $key }}" name="services[]"
-                                                                    value="{{ $service->id }}">
-                                                                {{ $service->service }}
-                                                            </label>
-                                                        @endif
-                                                    @endforeach
-
-                                                </div>
-                                            </div><!-- end col -->
-                                            <div class="col-12">
-                                                <x-form.ck-editor id="ck-editor-{{ $for }}"
-                                                    name="message"></x-form.ck-editor>
-                                            </div><!-- end col -->
-                                            <div class="d-flex">
-                                                <button type="submit"
-                                                    class="btn btn-primary waves-effect waves-light profile-button m-2">Submit</button>
-                                                <button type="submit" data-form="form-{{$index}}"
-                                                    class="apply btn btn-success waves-effect waves-light profile-button m-2">Submit
-                                                    & Apply for service</button>
-                                            </div>
                                         </div>
 
 
+                                        <div class="col-12 my-4">
+                                            <h4 class="mb-2">Tax Services</h4>
+                                            <div class="clearfix d-flex flex-wrap gap-3 align-items-center">
+                                                @php
+                                                    $services = \App\Models\TaxSetting::where([
+                                                        'for' => $for,
+                                                        'type' => 'others',
+                                                    ])->get(['service', 'id']);
+                                                @endphp
+                                                @foreach ($services as $key => $service)
+                                                    @if ($service)
+                                                        @php
+                                                            $id = uniqid('service-');
+                                                        @endphp
+                                                        <label x-data="{ selected: false }"
+                                                            :class="selected ? 'check-active' :
+                                                                'bg-light'"
+                                                            class="form-check-label d-flex gap-2 py-2 px-3 bg-gradient rounded-3"
+                                                            for="{{ $id }}">
+                                                            <input @change="selected = !selected; console.log(selected)"
+                                                                class="form-check-input" type="checkbox" hidden
+                                                                id="{{ $id }}" name="services[]"
+                                                                value="{{ $service->id }}">
+                                                            {{ $service->service }}
+                                                        </label>
+                                                    @endif
+                                                @endforeach
+
+                                            </div>
+                                        </div><!-- end col -->
+                                        <div class="col-12">
+                                            <x-form.ck-editor id="ck-editor-{{ $for }}"
+                                                name="message"></x-form.ck-editor>
+                                        </div><!-- end col -->
+                                        <div class="d-flex">
+                                            <button type="submit"
+                                                class="btn btn-primary waves-effect waves-light profile-button m-2">Submit</button>
+                                            <button type="submit" data-form="form-{{ $index }}"
+                                                class="apply btn btn-success waves-effect waves-light profile-button m-2">Submit
+                                                & Apply for service</button>
+                                        </div>
                                     </div>
                                 </form>
                             </div>
@@ -173,34 +168,21 @@
             </div>
         </section>
     </div>
-        <!-- end row-->
-    @endsection
+    <!-- end row-->
+@endsection
 
-    @push('customJs')
-        <script>
-            $(document).ready(function() {
-                $('.form-check-input').each((i, input)=>{
-                console.log(input)
-                    $(input).on('change', e => {
-                        $(input)
-                        .parent()
-                        .toggleClass('check-active')
-                        .toggleClass('bg-light');
-                    })
+@push('customJs')
+    <script>
+        $(document).ready(function() {
+            $(".apply").each(function(i, btn) {
+                $(btn).click((e) => {
+                    e.preventDefault()
+                    let url = "{{ route('tax.calculate') . '?apply=true' }}";
+                    let form = $("#" + $(btn).data('form'));
+                    form.attr('action', url)
+                    form.submit()
                 })
             });
-        </script>
-        <script>
-            $(document).ready(function() {
-                $(".apply").each(function(i, btn) {
-                    $(btn).click((e)=>{
-                        e.preventDefault()
-                        let url = "{{ route('tax.calculate').'?apply=true' }}";
-                        let form = $("#" + $(btn).data('form'));
-                        form.attr('action', url)
-                        form.submit()
-                    })
-                });
-            });
-        </script>
-    @endpush
+        });
+    </script>
+@endpush
